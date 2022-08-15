f96217cdf4b5e7fb0af4d7cd438a38273622fd04
Manuel Rossetti <rossetti@uark.edu>
2022-08-15 17:42:02 -0500

    Update Process.kt

--- src/main/kotlin/ksl/process/Process.kt
+++ src/main/kotlin/ksl/process/Process.kt
@@ -121,9 +121,185 @@ internal open class ProcessContinuation : Continuation<Unit> {
 fun main(){
 
     val p = ProcessCoroutine()// does not make a coroutine, yet
+
+    val s = Something("manuel")
+    println(s.name)
 }
+interface Person {
+    val name : String
+}
+public fun Something(str: String): Person = object : Person {
+    override val name: String
+        get() = str
+}
+
+/* Sequence takes in a function that makes an iterator, returns an instance of a Sequence
+ by creating an object instance that implements Sequence and uses the argument parameter
+ to define the iterator function of the object that is returned.
+*/
+public inline fun <T> Sequence(crossinline iteratorParameter: () -> Iterator<T>): Sequence<T> = object : Sequence<T> {
+    override fun iterator(): Iterator<T> = iteratorParameter()
+}
+
+/*
+The function sequence() takes in a suspending function with receiver (SequenceScope<T>) and returns an instance
+of the interface Sequence by defining a function declaration with equals that calls the function
+Sequence and passing in a lambda expression that calls the iterator function that uses the suspending function parameter
+"block" to create an Iterator
+ */
+public fun <T> sequence(block: suspend SequenceScope<T>.() -> Unit): Sequence<T> = Sequence { iterator(block) }
+
+public fun <T> iterator(block: suspend SequenceScope<T>.() -> Unit): Iterator<T> {
+    val iterator = SequenceBuilderIterator<T>()
+
+    iterator.nextStep = block.createCoroutineUnintercepted<SequenceBuilderIterator<T>, Unit>(receiver = iterator, completion = iterator)
+    return iterator
+}
+@RestrictsSuspension
+public abstract class SequenceScope<in T> internal constructor() {
+    /**
+     * Yields a value to the [Iterator] being built and suspends
+     * until the next value is requested.
+     *
+     * @sample samples.collections.Sequences.Building.buildSequenceYieldAll
+     * @sample samples.collections.Sequences.Building.buildFibonacciSequence
+     */
+    public abstract suspend fun yield(value: T)
+
+    /**
+     * Yields all values from the `iterator` to the [Iterator] being built
+     * and suspends until all these values are iterated and the next one is requested.
+     *
+     * The sequence of values returned by the given iterator can be potentially infinite.
+     *
+     * @sample samples.collections.Sequences.Building.buildSequenceYieldAll
+     */
+    public abstract suspend fun yieldAll(iterator: Iterator<T>)
+
+    /**
+     * Yields a collections of values to the [Iterator] being built
+     * and suspends until all these values are iterated and the next one is requested.
+     *
+     * @sample samples.collections.Sequences.Building.buildSequenceYieldAll
+     */
+    public suspend fun yieldAll(elements: Iterable<T>) {
+        if (elements is Collection && elements.isEmpty()) return
+        return yieldAll(elements.iterator())
+    }
+
+    /**
+     * Yields potentially infinite sequence of values  to the [Iterator] being built
+     * and suspends until all these values are iterated and the next one is requested.
+     *
+     * The sequence can be potentially infinite.
+     *
+     * @sample samples.collections.Sequences.Building.buildSequenceYieldAll
+     */
+    public suspend fun yieldAll(sequence: Sequence<T>) = yieldAll(sequence.iterator())
+}
+
+
+private typealias State = Int
+
+private const val State_NotReady: State = 0
+private const val State_ManyNotReady: State = 1
+private const val State_ManyReady: State = 2
+private const val State_Ready: State = 3
+private const val State_Done: State = 4
+private const val State_Failed: State = 5
+
+private class SequenceBuilderIterator<T> : SequenceScope<T>(), Iterator<T>, Continuation<Unit> {
+    // I think that this extends SequenceScope and Continuation so that it can be used as both receiver and completion parameters
+    private var state = State_NotReady
+    private var nextValue: T? = null
+    private var nextIterator: Iterator<T>? = null
+    var nextStep: Continuation<Unit>? = null // used to capture the Continuation for resumption after suspension or creation
+
+    override fun hasNext(): Boolean {
+        while (true) {
+            when (state) {
+                State_NotReady -> {}
+                State_ManyNotReady ->
+                    if (nextIterator!!.hasNext()) {
+                        state = State_ManyReady
+                        return true
+                    } else {
+                        nextIterator = null
+                    }
+                State_Done -> return false
+                State_Ready, State_ManyReady -> return true
+                else -> throw exceptionalState()
+            }
+
+            state = State_Failed
+            val step = nextStep!!
+            nextStep = null
+            step.resume(Unit)
+        }
+    }
+
+    override fun next(): T {
+        when (state) {
+            State_NotReady, State_ManyNotReady -> return nextNotReady()
+            State_ManyReady -> {
+                state = State_ManyNotReady
+                return nextIterator!!.next()
+            }
+            State_Ready -> {
+                state = State_NotReady
+                @Suppress("UNCHECKED_CAST")
+                val result = nextValue as T
+                nextValue = null
+                return result
+            }
+            else -> throw exceptionalState()
+        }
+    }
+
+    private fun nextNotReady(): T {
+        if (!hasNext()) throw NoSuchElementException() else return next()
+    }
+
+    private fun exceptionalState(): Throwable = when (state) {
+        State_Done -> NoSuchElementException()
+        State_Failed -> IllegalStateException("Iterator has failed.")
+        else -> IllegalStateException("Unexpected state of the iterator: $state")
+    }
+
+
+    override suspend fun yield(value: T) {
+        nextValue = value
+        state = State_Ready
+        return suspendCoroutineUninterceptedOrReturn { c ->
+            nextStep = c
+            COROUTINE_SUSPENDED
+        }
+    }
+
+    override suspend fun yieldAll(iterator: Iterator<T>) {
+        if (!iterator.hasNext()) return
+        nextIterator = iterator
+        state = State_ManyReady
+        return suspendCoroutineUninterceptedOrReturn { c ->
+            nextStep = c
+            COROUTINE_SUSPENDED
+        }
+    }
+
+    // Completion continuation implementation
+    override fun resumeWith(result: Result<Unit>) {
+        result.getOrThrow() // just rethrow exception if it is there
+        state = State_Done
+    }
+
+    override val context: CoroutineContext
+        get() = EmptyCoroutineContext
+}
+
+
+
 internal class ProcessCoroutine : ProcessScope, ProcessContinuation() {
-    lateinit var continuation : Continuation<Unit>
+    var continuation : Continuation<Unit>? = null //set with suspending
 
     override fun activate(atTime: Double, priority: Int): JSLEvent<Entity> {
         TODO("Not yet implemented")
@@ -131,7 +307,7 @@ internal class ProcessCoroutine : ProcessScope, ProcessContinuation() {
 
     override suspend fun resume() {
         // what to do if the process is not suspended
-        continuation.resume(Unit)
+        continuation?.resume(Unit)
         //TODO("Not yet implemented")
     }
 
@@ -142,7 +318,8 @@ internal class ProcessCoroutine : ProcessScope, ProcessContinuation() {
 //    }
 
     override suspend fun suspend() {
-        return suspendCoroutineUninterceptedOrReturn { cont ->
+        // whenever suspended this creates a new continuation, which must be captured for resumption
+        return suspendCoroutineUninterceptedOrReturn<Unit> { cont ->
             continuation = cont
             COROUTINE_SUSPENDED }
     }
