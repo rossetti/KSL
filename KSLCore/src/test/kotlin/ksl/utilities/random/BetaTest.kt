35ac9096f763fd52715c49518c5bdec0ecda4a95
Manuel Rossetti <rossetti@uark.edu>
2022-08-26 12:10:20 -0500

    Update BetaTest.kt

--- KSLCore/src/test/kotlin/ksl/utilities/random/BetaTest.kt
+++ KSLCore/src/test/kotlin/ksl/utilities/random/BetaTest.kt
@@ -1,10 +1,29 @@
 package ksl.utilities.random
 
+import jsl.utilities.random.rvariable.JSLRandom
+import ksl.utilities.random.rvariable.BetaRV
+import ksl.utilities.random.rvariable.KSLRandom
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 class BetaTest {
 
+    var bKSL: BetaRV? = null
+    var bJSL: jsl.utilities.random.rvariable.BetaRV? = null
     @BeforeEach
     fun setUp() {
+//        bJSL = jsl.utilities.random.rvariable.BetaRV(2.4, 5.5, 1)
+//        bKSL = BetaRV(2.4, 5.5, 1)
+    }
+
+    @Test
+    fun test1() {
+        for (i in 1..10){
+            val a = KSLRandom.rBeta(2.4, 5.5, 1)
+            val b = JSLRandom.rBeta(2.4, 5.5, 1)
+            println("KSL a = $a  JSL b = $b")
+            Assertions.assertTrue(a == b)
+        }
     }
 }
