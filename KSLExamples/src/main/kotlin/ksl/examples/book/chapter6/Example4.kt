80a6883e7fb77b64f15f2d11f4ef37307374cf2a
Manuel Rossetti <rossetti@uark.edu>
2022-08-26 09:26:18 -0500

    Update Example4.kt

--- KSLExamples/src/main/kotlin/ksl/examples/book/chapter6/Example4.kt
+++ KSLExamples/src/main/kotlin/ksl/examples/book/chapter6/Example4.kt
@@ -15,20 +15,12 @@ import ksl.utilities.random.rvariable.ExponentialRV
  */
 fun main() {
     val model = Model("Drive Through Pharmacy")
-    model.numberOfReplications = 2
-    model.lengthOfReplication = 20.0
-    model.lengthOfReplicationWarmUp = 5.0
+    model.numberOfReplications = 30
+    model.lengthOfReplication = 20000.0
+    model.lengthOfReplicationWarmUp = 5000.0
     // add DriveThroughPharmacy to the main model
     val dtp = DriveThroughPharmacy(model, 1)
     dtp.setTimeBtwArrivalRandomSource(ExponentialRV(6.0, 1))
     dtp.setServiceTimeRandomSource(ExponentialRV(3.0, 2))
-
-    val v = model.getResponse("# in System")
-    if (v != null){
-        val trace = ResponseTrace(v)
-    }
-
     model.simulate()
-//    sim.simulationReporter.printHalfWidthSummaryReport()
-    //TODO results do not match JSL model, twresponse slightly off, counter very off
 }
