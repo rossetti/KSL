6128cdebc0db0a4f893e5561845281c8d59a7948
Manuel Rossetti <rossetti@uark.edu>
2022-09-12 10:45:02 -0500

    Update SimpleProcessQ.kt

--- KSLCore/src/main/kotlin/examplepkg/SimpleProcessQ.kt
+++ KSLCore/src/main/kotlin/examplepkg/SimpleProcessQ.kt
@@ -4,6 +4,8 @@ import ksl.modeling.entity.EntityType
 import ksl.modeling.entity.KSLProcess
 import ksl.modeling.entity.Resource
 import ksl.modeling.variable.RandomVariable
+import ksl.modeling.variable.Response
+import ksl.modeling.variable.TWResponse
 import ksl.simulation.KSLEvent
 import ksl.simulation.Model
 import ksl.simulation.ModelElement
@@ -14,13 +16,19 @@ class SimpleProcessQ(parent: ModelElement) : EntityType(parent, null) {
     val worker: Resource = Resource(this, "worker")
     val tba = RandomVariable(this, ExponentialRV(6.0, 1))
     val st = RandomVariable(this, ExponentialRV(3.0, 2))
+    val wip = TWResponse(this, "${name}:WIP")
+    val tip = Response(this, "${name}:TimeInSystem")
     private val arrivals = Arrivals()
 
     private inner class Customer: Entity() {
         val mm1: KSLProcess = process("MM1"){
+            wip.increment()
+            timeStamp = time
             val a  = seize(worker)
             delay(st)
             release(a)
+            tip.value = time - timeStamp
+            wip.decrement()
         }
     }
 
