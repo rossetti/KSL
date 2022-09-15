67e3b4dba936be9d854f332ec1baa34b0d4675ba
Manuel Rossetti <rossetti@uark.edu>
2022-09-15 08:59:15 -0500

    Update RandomVariable.kt

--- KSLCore/src/main/kotlin/ksl/modeling/variable/RandomVariable.kt
+++ KSLCore/src/main/kotlin/ksl/modeling/variable/RandomVariable.kt
@@ -7,6 +7,53 @@ import ksl.utilities.random.RandomIfc
 import ksl.utilities.random.rng.RNStreamIfc
 
 /**
+ *  While RandomVariable instances should in general be declared as private within model
+ *  elements, this interface provides the modeler the ability to declare a public property
+ *  that returns an instance with limited ability to change and use the underlying RandomVariable,
+ *  prior to running the model.
+ *
+ *  For example:
+ *
+ *   private val myTBA = RandomVariable(this, ExponentialRV(6.0, 1))
+ *   val tba: RandomVariableIfc
+ *      get() = myTBA
+ *
+ *   Then users of the public property can change the initial random source and do other
+ *   controlled changes without fully exposing the private variable.  The implementer of the
+ *   model element that contains the private random variable does not have to write additional
+ *   functions to control the random variable and can use this strategy to expose what is needed.
+ *
+ */
+interface RandomVariableIfc {
+    var resetNextSubStreamOption: Boolean
+    var resetStartStreamOption: Boolean
+
+    /**
+     * RandomIfc provides a reference to the underlying source of randomness
+     * to initialize each replication.
+     * Controls the underlying RandomIfc source for the RandomVariable. This is the
+     * source to which each replication will be initialized.  This is only used
+     * when the replication is initialized. Changing the reference has no effect
+     * during a replication, since the random variable will continue to use
+     * the reference returned by property randomSource.  Please also see the
+     * discussion in the class documentation.
+     * <p>
+     * WARNING: If this is used during an experiment to change the characteristics of
+     * the random source, then each replication may not necessarily start in the
+     * same initial state.  It is recommended that this be used only prior to executing experiments.
+     */
+    var initialRandomSource: RandomIfc
+    var rnStream: RNStreamIfc
+
+    /**
+     * Controls whether warning of changing the initial random source during a replication
+     * is logged, default is true.
+     */
+    var initialRandomSourceChangeWarning: Boolean
+    fun asString(): String
+}
+
+/**
  * A random variable (RandomVariable) is a function that maps a probability space to a real number.
  * A random variable uses a RandomIfc to provide the underlying mapping to a real number via the value() method.
  * <p>
@@ -51,7 +98,8 @@ import ksl.utilities.random.rng.RNStreamIfc
  * is set to false.
  */
 class RandomVariable(parent: ModelElement, rSource: RandomIfc, name: String? = null) : ModelElement(parent, name),
-    RandomIfc, RandomElementIfc {
+    RandomIfc, RandomElementIfc, RandomVariableIfc {
+
     //TODO there is no capturing of the response implemented like in the JSL
 
     override var resetNextSubStreamOption: Boolean = true
@@ -72,7 +120,7 @@ class RandomVariable(parent: ModelElement, rSource: RandomIfc, name: String? = n
      * the random source, then each replication may not necessarily start in the
      * same initial state.  It is recommended that this be used only prior to executing experiments.
      */
-    var initialRandomSource: RandomIfc = rSource
+    override var initialRandomSource: RandomIfc = rSource
         set(value) {
             if (model.isRunning){
                 if (initialRandomSourceChangeWarning){
@@ -107,7 +155,7 @@ class RandomVariable(parent: ModelElement, rSource: RandomIfc, name: String? = n
      * Controls whether warning of changing the initial random source during a replication
      * is logged, default is true.
      */
-    var initialRandomSourceChangeWarning = true
+    override var initialRandomSourceChangeWarning = true
 
     init {
         warmUpOption = false
@@ -152,7 +200,7 @@ class RandomVariable(parent: ModelElement, rSource: RandomIfc, name: String? = n
         }
     }
 
-    fun asString(): String {
+    override fun asString(): String {
         val sb = StringBuilder()
         sb.append(toString())
         sb.append(randomSource.toString())
