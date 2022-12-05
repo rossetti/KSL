b7dc9d629bf759983f1ec757fe5e63c0063e0fc5
Manuel Rossetti <rossetti@uark.edu>
2022-12-04 19:31:21 -0600

    Update Example6.kt

--- KSLCore/src/main/kotlin/ksl/examples/book/chapter5/Example6.kt
+++ KSLCore/src/main/kotlin/ksl/examples/book/chapter5/Example6.kt
@@ -6,6 +6,12 @@ import ksl.simulation.Model
 import ksl.utilities.io.dbutil.KSLDatabaseObserver
 
 fun main() {
+
+    withoutCommonRandomNumbers()
+    withCommonRandomNumbers()
+}
+
+fun withoutCommonRandomNumbers(){
     val model = Model("Pallet Model Experiments")
     model.numberOfReplications = 30
     // add the model element to the main model
@@ -15,6 +21,35 @@ fun main() {
     // simulate the model
     model.experimentName = "One Worker"
     palletWorkCenter.numWorkers = 1
+    model.simulate()
+
+    model.experimentName = "Two Workers"
+    palletWorkCenter.numWorkers = 2
+    model.simulate()
+
+    model.experimentName = "Three Workers"
+    palletWorkCenter.numWorkers = 3
+    model.simulate()
+
+    val responseName = palletWorkCenter.totalProcessingTime.name
+    val db = kslDatabaseObserver.db
+
+    val expNames = listOf("One Worker","Two Workers", "Three Workers")
+    val comparisonAnalyzer = db.multipleComparisonAnalyzerFor(expNames, responseName)
+    println(comparisonAnalyzer)
+}
+
+fun withCommonRandomNumbers(){
+    val model = Model("Pallet Model Experiments")
+    model.numberOfReplications = 30
+    // add the model element to the main model
+    val palletWorkCenter = PalletWorkCenter(model)
+    // use a database to capture the response data
+    val kslDatabaseObserver = KSLDatabaseObserver(model)
+    // simulate the model
+    model.experimentName = "One Worker"
+    palletWorkCenter.numWorkers = 1
+    model.resetStartStreamOption = true
     model.simulate()
 
     model.experimentName = "Two Workers"
