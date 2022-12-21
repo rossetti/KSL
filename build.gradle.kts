60b98127d551093a0327969a2adede36490747ef
Manuel Rossetti <rossetti@uark.edu>
2022-12-21 16:59:58 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     `java-library`
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.7.20"
 }
 group = "io.github.rossetti"
 version = "1.0-SNAPSHOT"
