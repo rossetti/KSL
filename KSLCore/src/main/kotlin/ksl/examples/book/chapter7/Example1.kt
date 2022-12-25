dc7c92f8625d1de1fc6800b4df49fb9e823877a8
Manuel Rossetti <rossetti@uark.edu>
2022-12-25 13:54:48 -0500

    tandem queue

--- KSLCore/src/main/kotlin/ksl/examples/book/chapter7/Example1.kt
+++ KSLCore/src/main/kotlin/ksl/examples/book/chapter7/Example1.kt
@@ -1,4 +1,13 @@
 package ksl.examples.book.chapter7
 
-class Example1 {
+import ksl.simulation.Model
+
+fun main(){
+    val m = Model()
+    val tq = TandemQueue(m, name = "TandemQModel")
+    m.numberOfReplications = 30
+    m.lengthOfReplication = 20000.0
+    m.lengthOfReplicationWarmUp = 5000.0
+    m.simulate()
+    m.print()
 }
