ce72994a4247a41ad6d521dc33d345bd109c1386
Manuel Rossetti <rossetti@uark.edu>
2023-04-09 17:40:41 -0500

    Update JobShop.kt

--- KSLExamples/src/main/kotlin/ksl/examples/general/models/jobshop/JobShop.kt
+++ KSLExamples/src/main/kotlin/ksl/examples/general/models/jobshop/JobShop.kt
@@ -1,11 +1,16 @@
 package ksl.examples.general.models.jobshop
 
 import ksl.examples.book.chapter7.TestAndRepairShop
+import ksl.modeling.elements.EventGeneratorCIfc
 import ksl.modeling.elements.REmpiricalList
+import ksl.modeling.entity.KSLProcess
 import ksl.modeling.entity.ProcessModel
 import ksl.modeling.entity.ResourceWithQ
-import ksl.modeling.variable.RandomVariable
+import ksl.modeling.variable.*
+import ksl.simulation.Model
 import ksl.simulation.ModelElement
+import ksl.utilities.io.KSL
+import ksl.utilities.io.MarkDown
 import ksl.utilities.random.RandomIfc
 import ksl.utilities.random.rvariable.ExponentialRV
 import ksl.utilities.random.rvariable.GammaRV
@@ -22,33 +27,82 @@ class JobShop(parent: ModelElement, name: String? = null) : ProcessModel(parent,
     private val machine5: ResourceWithQ = ResourceWithQ(this, capacity = 1, name = "Machine5")
 
     // define steps to represent a sequence
-    inner class JobStep(val resource: ResourceWithQ, random: RandomIfc) {
+    private inner class JobStep(val resource: ResourceWithQ, random: RandomIfc) {
         val processTime = RandomVariable(this@JobShop, random)
     }
 
-    val jobSeq1 = listOf<JobStep>(
-        JobStep(machine3, GammaRV(2.0, 0.5 / 2.0)),
-        JobStep(machine1, GammaRV(2.0, 0.6 / 2.0)),
-        JobStep(machine2, GammaRV(2.0, 0.85 / 2.0)),
-        JobStep(machine5, GammaRV(2.0, 0.5 / 2.0))
+    private val jobSeq1 = listOf<JobStep>(
+        JobStep(machine3, GammaRV(2.0, 0.5 / 2.0, 2)),
+        JobStep(machine1, GammaRV(2.0, 0.6 / 2.0, 3)),
+        JobStep(machine2, GammaRV(2.0, 0.85 / 2.0, 4)),
+        JobStep(machine5, GammaRV(2.0, 0.5 / 2.0, 5))
     )
 
-    val jobSeq2 = listOf<JobStep>(
-        JobStep(machine4, GammaRV(2.0, 1.1 / 2.0)),
-        JobStep(machine1, GammaRV(2.0, 0.8 / 2.0)),
-        JobStep(machine3, GammaRV(2.0, 0.75 / 2.0))
+    private val jobSeq2 = listOf<JobStep>(
+        JobStep(machine4, GammaRV(2.0, 1.1 / 2.0, 6)),
+        JobStep(machine1, GammaRV(2.0, 0.8 / 2.0, 7)),
+        JobStep(machine3, GammaRV(2.0, 0.75 / 2.0, 8))
     )
 
-    val jobSeq3 = listOf<JobStep>(
-        JobStep(machine2, GammaRV(2.0, 1.2 / 2.0)),
-        JobStep(machine5, GammaRV(2.0, 0.25 / 2.0)),
-        JobStep(machine1, GammaRV(2.0, 0.7 / 2.0)),
-        JobStep(machine4, GammaRV(2.0, 0.9 / 2.0)),
-        JobStep(machine3, GammaRV(2.0, 1.0 / 2.0))
+    private val jobSeq3 = listOf<JobStep>(
+        JobStep(machine2, GammaRV(2.0, 1.2 / 2.0, 9)),
+        JobStep(machine5, GammaRV(2.0, 0.25 / 2.0, 10)),
+        JobStep(machine1, GammaRV(2.0, 0.7 / 2.0, 11)),
+        JobStep(machine4, GammaRV(2.0, 0.9 / 2.0, 12)),
+        JobStep(machine3, GammaRV(2.0, 1.0 / 2.0, 13))
     )
 
     // set up the sequences and the random selection of the plan
     private val sequences = listOf(jobSeq1, jobSeq2, jobSeq3)
     private val seqCDF = doubleArrayOf(0.3, 0.8, 1.0)
     private val seqList = REmpiricalList<List<JobStep>>(this, sequences, seqCDF)
+
+    private val myArrivalGenerator = EntityGenerator(::Job, myTBA, myTBA)
+    val generator: EventGeneratorCIfc
+        get() = myArrivalGenerator
+
+    // define the responses
+    private val wip: TWResponse = TWResponse(this, "${this.name}:NumInSystem")
+    val numInSystem: TWResponseCIfc
+        get() = wip
+    private val timeInSystem: Response = Response(this, "${this.name}:TimeInSystem")
+    val systemTime: ResponseCIfc
+        get() = timeInSystem
+
+    private val systemTimeByType = mapOf<List<JobStep>, Response>(
+        jobSeq1 to Response(this, "Type1:TimeInSystem"),
+        jobSeq2 to Response(this, "Type2:TimeInSystem"),
+        jobSeq3 to Response(this, "Type3:TimeInSystem"),
+    )
+
+    private inner class Job : Entity() {
+        val jobShopProcess: KSLProcess = process {
+            wip.increment()
+            timeStamp = time
+            // determine the job sequence
+            val sequence: List<JobStep> = seqList.element
+            // get the iterator
+            val itr = sequence.iterator()
+            // iterate through the sequence
+            while (itr.hasNext()) {
+                val jobStep = itr.next()
+                val a = seize(jobStep.resource)
+                delay(jobStep.processTime)
+                release(a)
+            }
+            val systemTime = time - timeStamp
+            timeInSystem.value = systemTime
+            systemTimeByType[sequence]!!.value = systemTime
+            wip.decrement()
+        }
+    }
+}
+
+fun main() {
+    val m = Model("Job Shop Model")
+    JobShop(m, "JobShop")
+    m.numberOfReplications = 500
+    m.lengthOfReplication = 365.0 * 8.0
+    m.simulate()
+    m.print()
 }
