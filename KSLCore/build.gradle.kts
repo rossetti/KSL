525a13457cbc6a907e639bab5bde2a74b1983b27
Manuel Rossetti <rossetti@uark.edu>
2023-07-30 15:12:30 -0500

    Update build.gradle.kts

--- KSLCore/build.gradle.kts
+++ KSLCore/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     api("org.jetbrains.kotlinx:dataframe-core:0.11.0")
 
 //    implementation("org.junit.jupiter:junit-jupiter:5.9.0")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.0")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.0")
 
     // https://mvnrepository.com/artifact/org.ktorm/ktorm-core
 //    implementation("org.ktorm:ktorm-core:3.5.0")
