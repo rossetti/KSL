e94e3b5b7a1d509778551c5782f54b39031b034a
Manuel Rossetti <rossetti@uark.edu>
2023-08-27 19:04:29 -0500

    some updates

--- KSLCore/src/main/kotlin/ksl/utilities/io/plotting/Plot.kt
+++ KSLCore/src/main/kotlin/ksl/utilities/io/plotting/Plot.kt
@@ -2,11 +2,13 @@ package ksl.utilities.io.plotting
 
 import org.jetbrains.letsPlot.geom.geomPoint
 import org.jetbrains.letsPlot.ggplot
+import org.jetbrains.letsPlot.intern.Plot
+import org.jetbrains.letsPlot.label.ggtitle
 import java.awt.Desktop
 import java.io.File
 import java.nio.file.Path
 
-abstract class PlotImp(override var title: String? = null) : PlotIfc {
+abstract class PlotImp(override var title: String = "") : PlotIfc {
 
     override var defaultScale: Int = 1
         set(value) {
@@ -34,11 +36,11 @@ abstract class PlotImp(override var title: String? = null) : PlotIfc {
         desktop.browse(file.toURI())
     }
 
-    override fun saveToFile(path: Path, plotTitle: String?): File {
+    override fun saveToFile(path: Path, plotTitle: String): File {
         TODO("Not yet implemented")
     }
 
-    override fun showInBrowser(plotTitle: String?): File {
+    override fun showInBrowser(plotTitle: String): File {
         TODO("Not yet implemented")
     }
 
@@ -47,26 +49,28 @@ abstract class PlotImp(override var title: String? = null) : PlotIfc {
 internal class ScatterPlot(
     x: DoubleArray,
     y: DoubleArray,
-    val xLabel: String = "x",
-    val yLabel: String = "y",
-    title: String? = null
+    private val xLabel: String = "x",
+    private val yLabel: String = "y",
+    title: String = ""
 ) : PlotImp(title) {
 
     private val data: Map<String, DoubleArray>
-    init{
+
+    init {
         data = mapOf(
             xLabel to x,
             yLabel to y
         )
     }
 
-    private fun buildPlot(){
+    private fun buildPlot(): Plot {
         val p = ggplot(data) +
-                geomPoint(){
+                geomPoint() {
                     x = xLabel
                     y = yLabel
-                }
-
+                } +
+                ggtitle(title)
+        return p
     }
 }
 
--- KSLCore/src/main/kotlin/ksl/utilities/io/plotting/PlottingIfc.kt
+++ KSLCore/src/main/kotlin/ksl/utilities/io/plotting/PlottingIfc.kt
@@ -21,39 +21,39 @@ interface PlotIfc {
     var defaultDPI : Int
     var width: Int
     var height: Int
-    var title: String?
+    var title: String
 
-    fun saveToFile(path: Path, plotTitle: String? = null) : File
+    fun saveToFile(path: Path, plotTitle: String = title) : File
 
-    fun saveToFile(fileName: String, directory: Path = KSL.outDir, plotTitle: String? = title) : File {
+    fun saveToFile(fileName: String, directory: Path = KSL.outDir, plotTitle: String = title) : File {
         return saveToFile(directory.resolve(fileName), plotTitle)
     }
 
-    fun showInBrowser(plotTitle: String? = title): File
+    fun showInBrowser(plotTitle: String = title): File
 
 }
 
 interface PlottingIfc {
 
-    fun scatterPlot(x: DoubleArray, y: DoubleArray, title: String? = null): PlotIfc
+    fun scatterPlot(x: DoubleArray, y: DoubleArray, title: String = ""): PlotIfc
 
-    fun boxPlot(boxPlotSummary: BoxPlotSummary, title: String? = null): PlotIfc
+    fun boxPlot(boxPlotSummary: BoxPlotSummary, title: String = ""): PlotIfc
 
-    fun multiBoxPlot(list: List<BoxPlotSummary>, title: String? = null): PlotIfc
+    fun multiBoxPlot(list: List<BoxPlotSummary>, title: String = ""): PlotIfc
 
-    fun histogram(histogram: Histogram, title: String? = null, density: Boolean = false): PlotIfc
+    fun histogram(histogram: Histogram, title: String = "", density: Boolean = false): PlotIfc
 
-    fun frequency(frequency: IntegerFrequency, title: String? = null): PlotIfc
+    fun frequency(frequency: IntegerFrequency, title: String = ""): PlotIfc
 
-    fun frequency(frequency: StateFrequency, title: String? = null): PlotIfc
+    fun frequency(frequency: StateFrequency, title: String = ""): PlotIfc
 
-    fun confidenceIntervals(list: List<Interval>, title: String? = null, referencePoint: Double = Double.NaN): PlotIfc
+    fun confidenceIntervals(list: List<Interval>, title: String = "", referencePoint: Double = Double.NaN): PlotIfc
 
     fun functionPlot(function: FunctionIfc, interval: Interval, mesh: Double = 0.0): PlotIfc
 
-    fun qqPlot(data: DoubleArray, hypothesized: ContinuousDistributionIfc, title: String? = null): PlotIfc
+    fun qqPlot(data: DoubleArray, hypothesized: ContinuousDistributionIfc, title: String = ""): PlotIfc
 
-    fun ppPlot(data: DoubleArray, hypothesized: ContinuousDistributionIfc, title: String? = null): PlotIfc
+    fun ppPlot(data: DoubleArray, hypothesized: ContinuousDistributionIfc, title: String = ""): PlotIfc
 
     fun stateVariablePlot(): PlotIfc
 
@@ -61,7 +61,7 @@ interface PlottingIfc {
 
     fun welchPlot(dataArrayObserver: WelchDataArrayObserver): PlotIfc
 
-    fun welchPlot(averages: DoubleArray, cumAverages: DoubleArray, title: String? = null, responseName: String? = null): PlotIfc
+    fun welchPlot(averages: DoubleArray, cumAverages: DoubleArray, title: String = "", responseName: String? = null): PlotIfc
 
-    fun partialSumsPlot(partialSums: DoubleArray, title: String? = null, responseName: String? = null ): PlotIfc
+    fun partialSumsPlot(partialSums: DoubleArray, title: String = "", responseName: String? = null ): PlotIfc
 }
