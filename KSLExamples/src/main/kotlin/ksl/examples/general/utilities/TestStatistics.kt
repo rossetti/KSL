4df96b923967bf7de6096ce59aa62f9948861fb4
Manuel Rossetti <rossetti@uark.edu>
2023-09-06 11:40:20 -0500

    Update TestStatistics.kt

--- KSLExamples/src/main/kotlin/ksl/examples/general/utilities/TestStatistics.kt
+++ KSLExamples/src/main/kotlin/ksl/examples/general/utilities/TestStatistics.kt
@@ -45,13 +45,28 @@ class TestStatistics {
 }
 
 fun main(){
-    testData.writeToFile("testData.txt")
-    testAutoCorrelation()
+ //   testData.writeToFile("testData.txt")
+ //   testAutoCorrelation()
+    testPearsonCorrelation()
 }
 
 
 fun testPearsonCorrelation() {
+   val wt = doubleArrayOf(2.620, 2.875, 2.320, 3.215, 3.440, 3.460, 3.570, 3.190, 3.150, 3.440, 3.440,
+       4.070, 3.730, 3.780, 5.250, 5.424, 5.345, 2.200, 1.615, 1.835, 2.465, 3.520, 3.435, 3.840, 3.845, 1.935, 
+       2.140, 1.513, 3.170, 2.770, 3.570, 2.780)
+    val mpg = doubleArrayOf(
+        21.0, 21.0, 22.8, 21.4, 18.7, 18.1, 14.3, 24.4, 22.8, 19.2, 17.8, 16.4, 17.3, 15.2, 10.4, 10.4, 14.7,
+        32.4, 30.4, 33.9, 21.5, 15.5, 15.2, 13.3, 19.2, 27.3, 26.0, 30.4, 15.8, 19.7, 15.0, 21.4
+    )
+    val pc = Statistic.pearsonCorrelation(wt, mpg)
+    println("pearson correlation = $pc")
 
+    val x1 = testData.dropLast(1).toDoubleArray()
+    val x2 = testData.copyOfRange(1, testData.size)
+
+    val pc2 = Statistic.pearsonCorrelation(x1, x2)
+    println("pearson correlation = $pc2")
 }
 
 fun testAutoCorrelation() {
