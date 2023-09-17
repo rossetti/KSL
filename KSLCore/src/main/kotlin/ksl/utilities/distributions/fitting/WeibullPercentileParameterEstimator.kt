18fdb55a7c854ca1b7f454f6537fd12ec11156bd
Manuel Rossetti <rossetti@uark.edu>
2023-09-17 18:31:38 -0500

    Create WeibullPercentileParameterEstimator.kt

---
+++ KSLCore/src/main/kotlin/ksl/utilities/distributions/fitting/WeibullPercentileParameterEstimator.kt
@@ -0,0 +1,31 @@
+package ksl.utilities.distributions.fitting
+
+import ksl.utilities.countLessThan
+import ksl.utilities.isAllEqual
+import ksl.utilities.orderStatistics
+
+object WeibullPercentileParameterEstimator : ParameterEstimatorIfc {
+    override fun estimate(data: DoubleArray): EstimatedParameters {
+        if (data.size < 2){
+            return EstimatedParameters(
+                message = "There must be at least two observations",
+                success = false
+            )
+        }
+        if (data.countLessThan(0.0) > 0) {
+            return EstimatedParameters(
+                null,
+                message = "Cannot fit Weibull distribution when some observations are less than 0.0",
+                success = false
+            )
+        }
+        if (data.isAllEqual()){
+            return EstimatedParameters(
+                message = "Cannot estimate parameters.  The observations were all equal.",
+                success = false
+            )
+        }
+        val sorted = data.orderStatistics()
+        TODO("Not implemented yet")
+    }
+}
