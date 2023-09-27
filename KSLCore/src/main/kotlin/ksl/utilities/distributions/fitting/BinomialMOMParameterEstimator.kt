62cdbd4c7e226c8f3c20fea63e573e3ec7172a76
Manuel Rossetti <rossetti@uark.edu>
2023-09-27 13:04:17 -0500

    binomial work

---
+++ KSLCore/src/main/kotlin/ksl/utilities/distributions/fitting/BinomialMOMParameterEstimator.kt
@@ -0,0 +1,76 @@
+package ksl.utilities.distributions.fitting
+
+import ksl.utilities.countLessThan
+import ksl.utilities.random.rvariable.parameters.BinomialRVParameters
+import ksl.utilities.random.rvariable.parameters.NegativeBinomialRVParameters
+import ksl.utilities.statistic.StatisticIfc
+
+/**
+ *  Estimates the probability of success (p) and number of trials (n)
+ *  for the binomial distribution based on
+ *  the method of moments. The data must not contain negative values. The estimation
+ *  process assumes that the supplied data are integer valued counts
+ *  over the range {0,1,2,..., n}.  That is this parameterization represents
+ *  the number of success in n trials.
+ *  To ensure moment matching, the estimation process does not ensure that n is integer valued.
+ */
+object BinomialMOMParameterEstimator : ParameterEstimatorIfc {
+
+    override val checkRange: Boolean = true
+
+    override fun estimate(data: DoubleArray, statistics: StatisticIfc): EstimationResult {
+        if (data.size < 2) {
+            return EstimationResult(
+                originalData = data,
+                statistics = statistics,
+                message = "There must be at least two observations",
+                success = false
+            )
+        }
+        if (data.countLessThan(0.0) > 0) {
+            return EstimationResult(
+                originalData = data,
+                statistics = statistics,
+                message = "Cannot fit binomial distribution when some observations are less than 0.0",
+                success = false
+            )
+        }
+        if (statistics.average <= 0.0) {
+            return EstimationResult(
+                originalData = data,
+                statistics = statistics,
+                message = "The sample average of the data was <= 0.0",
+                success = false
+            )
+        }
+        if (statistics.variance <= 0.0) {
+            return EstimationResult(
+                originalData = data,
+                statistics = statistics,
+                message = "The sample variance of the data was <= 0.0",
+                success = false
+            )
+        }
+        val sigma2 = statistics.variance * (statistics.count - 1.0) / statistics.count
+        if (statistics.average <= sigma2) {
+            return EstimationResult(
+                originalData = data,
+                statistics = statistics,
+                message = "Cannot match moments when sample average <= estimated variance",
+                success = false
+            )
+        }
+        val p = 1.0 - (sigma2 / statistics.average)
+        val n = statistics.average / p
+        val parameters = BinomialRVParameters()
+        parameters.changeDoubleParameter("probOfSuccess", p)
+        parameters.changeDoubleParameter("numTrials", n)
+        return EstimationResult(
+            originalData = data,
+            statistics = statistics,
+            parameters = parameters,
+            message = "The binomial parameters were estimated successfully using a MOM technique",
+            success = true
+        )
+    }
+}
--- KSLCore/src/main/kotlin/ksl/utilities/random/rvariable/parameters/BinomialRVParameters.kt
+++ KSLCore/src/main/kotlin/ksl/utilities/random/rvariable/parameters/BinomialRVParameters.kt
@@ -6,23 +6,28 @@ import ksl.utilities.random.rvariable.BinomialRV
 import ksl.utilities.random.rvariable.RVType
 import ksl.utilities.random.rvariable.RVariableIfc
 
+/**
+ *  If the number of trials parameter (numTrials) is a double value,
+ *  it will be truncated to the nearest integer value when creating
+ *  the corresponding random variable or distribution.
+ */
 class BinomialRVParameters : RVParameters(), CreateDistributionIfc<Binomial> {
     override fun fillParameters() {
         addDoubleParameter("probOfSuccess", 0.5)
-        addIntegerParameter("numTrials", 2)
+        addDoubleParameter("numTrials", 2.0)
         rvClassName = RVType.Binomial.parametrizedRVClass.simpleName!!
         rvType = (RVType.Binomial)
     }
 
     override fun createRVariable(rnStream: RNStreamIfc): RVariableIfc {
         val probOfSuccess = doubleParameter("probOfSuccess")
-        val numTrials = integerParameter("numTrials")
-        return BinomialRV(probOfSuccess, numTrials, rnStream)
+        val numTrials = doubleParameter("numTrials")
+        return BinomialRV(probOfSuccess, numTrials.toInt(), rnStream)
     }
 
     override fun createDistribution(): Binomial {
         val probOfSuccess = doubleParameter("probOfSuccess")
-        val numTrials = integerParameter("numTrials")
-        return Binomial(probOfSuccess, numTrials)
+        val numTrials = doubleParameter("numTrials")
+        return Binomial(probOfSuccess, numTrials.toInt())
     }
 }
