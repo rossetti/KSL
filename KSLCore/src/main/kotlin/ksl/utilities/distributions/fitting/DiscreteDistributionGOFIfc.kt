ac3f28a10e55ad1b5d09dd80e7d93c8adf673a44
Manuel Rossetti <rossetti@uark.edu>
2023-10-09 08:27:21 -0500

    Update DiscreteDistributionGOFIfc.kt

--- KSLCore/src/main/kotlin/ksl/utilities/distributions/fitting/DiscreteDistributionGOFIfc.kt
+++ KSLCore/src/main/kotlin/ksl/utilities/distributions/fitting/DiscreteDistributionGOFIfc.kt
@@ -4,9 +4,9 @@ import ksl.utilities.distributions.DiscretePMFInRangeDistributionIfc
 import ksl.utilities.statistic.HistogramIfc
 
 interface DiscreteDistributionGOFIfc {
-    val numEstimatedParameters: Int
 
     val distribution: DiscretePMFInRangeDistributionIfc
+    val numEstimatedParameters: Int
     val histogram: HistogramIfc
     val breakPoints: DoubleArray
     val binProbabilities: DoubleArray
@@ -48,14 +48,13 @@ interface DiscreteDistributionGOFIfc {
         require((0.0 < type1Error) && (type1Error < 1.0)) { "Type 1 error must be in (0,1)" }
         val sb = StringBuilder()
         sb.appendLine("Chi-Squared Test Results:")
-        sb.append(String.format("%-20s %-10s %10s %10s", "Bin Range", "P(Bin)", "Observed", "Expected"))
+        sb.append(String.format("%-25s %-10s %10s %10s", "Bin Label", "P(Bin)", "Observed", "Expected"))
         sb.appendLine()
         for ((i, bin) in histogram.bins.withIndex()) {
-            val r = bin.openIntRange
             val o = bin.count
             val e = expectedCounts[i]
             val p = binProbabilities[i]
-            val s = String.format("%-20s %-10f %10d %10f", r, p, o, e)
+            val s = String.format("%-25s %-10f %10d %10f", bin.binLabel, p, o, e)
             sb.append(s)
             if (e <= 5){
                 sb.append("\t *** Warning: expected <= 5 ***")
@@ -68,7 +67,7 @@ interface DiscreteDistributionGOFIfc {
         sb.appendLine("Degrees of Freedom = $dof")
         sb.appendLine("Chi-Squared Test Statistic = $chiSquaredTestStatistic")
         sb.appendLine("P-value = $chiSquaredPValue")
-        sb.appendLine("Hypothesis test of ${distribution}, at $type1Error level: ")
+        sb.appendLine("Hypothesis test at $type1Error level: ")
         if (chiSquaredPValue >= type1Error){
             sb.appendLine("The p-value = $chiSquaredPValue is >= $type1Error : Do not reject hypothesis.")
         } else {
