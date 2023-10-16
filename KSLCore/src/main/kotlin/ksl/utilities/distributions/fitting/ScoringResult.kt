fa32d0883c93eb77cf41d08ac630c11c71913d53
Manuel Rossetti <rossetti@uark.edu>
2023-10-16 17:44:30 -0500

    updating PDFModeler

--- KSLCore/src/main/kotlin/ksl/utilities/distributions/fitting/ScoringResult.kt
+++ KSLCore/src/main/kotlin/ksl/utilities/distributions/fitting/ScoringResult.kt
@@ -6,12 +6,23 @@ import ksl.utilities.moda.MetricIfc
 import ksl.utilities.moda.Score
 
 data class ScoringResult(
+    val name: String,
     val distribution: ContinuousDistributionIfc,
     val estimationResult: EstimationResult,
-    val scores: List<Score>,
-    val values: List<Double>,
-    var totalValue: Double = 0.0
-) {
+    val scores: List<Score>
+) : Comparable<ScoringResult> {
 
     val metrics: List<MetricIfc> = MODAModel.extractMetrics(scores)
+
+    var values: Map<MetricIfc, Double> = emptyMap()
+        internal set
+
+    var weightedValue: Double = 0.0
+        internal set
+
+    override fun compareTo(other: ScoringResult): Int = compareBy(
+        ScoringResult::weightedValue,
+        ScoringResult::name
+    ).compare(this, other)
+
 }
