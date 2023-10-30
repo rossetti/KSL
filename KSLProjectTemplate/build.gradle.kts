34e5c216d4a9ab0cd87ecd1da27a2fdc0925c55d
Manuel Rossetti <rossetti@uark.edu>
2023-10-30 16:56:25 -0500

    release R1.0.2 work

--- KSLProjectTemplate/build.gradle.kts
+++ KSLProjectTemplate/build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     `java-library`
-    kotlin("jvm") version "1.7.20"
+    kotlin("jvm") version "1.9.0"
 }
 
 repositories {
@@ -17,7 +17,7 @@ dependencies {
 
     // next line allows use of KSL libraries within the project
     // update the release number when new releases become available
-    api(group = "io.github.rossetti", name = "KSLCore", version = "R1.0.1")
+    api(group = "io.github.rossetti", name = "KSLCore", version = "R1.0.2")
 
 
     testImplementation(kotlin("test"))
@@ -29,7 +29,7 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "17"
 }
 
 tasks.jar {
@@ -38,10 +38,10 @@ tasks.jar {
 
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
-    jvmTarget = "11"
+    jvmTarget = "17"
 }
 
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
-    jvmTarget = "11"
+    jvmTarget = "17"
 }
