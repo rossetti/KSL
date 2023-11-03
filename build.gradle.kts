76c02bffab648a314c363455fcfd90b464d0224a
Manuel Rossetti <rossetti@uark.edu>
2023-11-03 09:47:27 -0500

    clean up after latest release

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,13 +4,17 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     `java-library`
-    kotlin("jvm") version "1.7.20"
+    // uncomment for publishing task
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.serialization") version "1.9.0"
+    //   id("org.jetbrains.kotlinx.dataframe") version "0.11.0"
+    id("org.jetbrains.dokka") version "1.9.10"
 }
+
 group = "io.github.rossetti"
-version = "1.0-SNAPSHOT"
+version = "R1.0.2"
 
 buildscript {
-    repositories { jcenter() }
 
     dependencies {
         classpath("com.netflix.nebula:gradle-aggregate-javadocs-plugin:2.2.+")
@@ -20,60 +24,56 @@ buildscript {
 apply(plugin = "nebula-aggregate-javadocs")
 
 repositories {
-    jcenter()
     mavenCentral()
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.dokka")
 }
 
 dependencies {
 
     api(project(":KSLCore"))
     api(project(":KSLExamples"))
-    api(project(":KSLExtensions"))
-    
-//    api(group = "io.github.rossetti", name = "JSLCore", version = "R1.0.12")
-//    api(group = "io.github.rossetti", name = "JSLExtensions", version = "R1.0.12")
 
-    // https://mvnrepository.com/artifact/io.github.microutils/kotlin-logging-jvm
-//    api(group = "io.github.microutils", name = "kotlin-logging-jvm", version = "2.1.21")
-
-    // https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
-//    api(group = "ch.qos.logback", name = "logback-classic", version = "1.2.10")
-    // https://mvnrepository.com/artifact/ch.qos.logback/logback-core
-//    api(group = "ch.qos.logback", name = "logback-core", version = "1.2.10")
-
-//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-//    implementation("org.junit.jupiter:junit-jupiter:5.7.0")
-//    implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.10")
-
-//    testImplementation(kotlin("test"))
     implementation(kotlin("stdlib-jdk8"))
 }
 
-//tasks.test {
-//    useJUnitPlatform()
-//}
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "17"
+}
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "17"
 }
 
 // this is supposed to exclude the logback.xml resource file from the generated jar
 // this is good because user can then provide their own logging specification
 // TODO need reference to why this is good
-//tasks.jar {
+tasks.jar {
 //    manifest {
 //        attributes(
 //                "Implementation-Title" to project.name,
 //                "Implementation-Version" to project.version
 //        )
 //    }
-//    exclude("logback.xml")
-//}
+    exclude("logback.xml")
+}
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "17"
 }
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "17"
+}
+
+// these extensions are needed when publishing to maven
+// because maven requires javadoc jar, sources jar, and the build jar
+// these jars are placed in build/libs by default
+java {
+    // comment this out to not make jar file with javadocs during normal build
+    withJavadocJar()
+    // comment this out to not make jar file with source during normal build
+    withSourcesJar()
 }
