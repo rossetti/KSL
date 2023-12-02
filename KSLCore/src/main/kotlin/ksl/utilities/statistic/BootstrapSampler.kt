4798a3d7f1f494866021a6511ecc623cf6da3caa
Manuel Rossetti <rossetti@uark.edu>
2023-12-02 17:13:09 -0600

    more bs

--- KSLCore/src/main/kotlin/ksl/utilities/statistic/BootstrapSampler.kt
+++ KSLCore/src/main/kotlin/ksl/utilities/statistic/BootstrapSampler.kt
@@ -101,7 +101,7 @@ open class BootstrapSampler(
     /**
      * @return the estimate from the supplied MVBSEstimatorIfc based on the original data
      */
-    val originalDataEstimates = estimator.estimate(originalData)
+    val originalDataEstimate = estimator.estimate(originalData)
 
     // use to perform the sampling from the original data
     protected val myOriginalPop: DPopulation = DPopulation(originalData, stream)
@@ -248,7 +248,7 @@ open class BootstrapSampler(
         // now process the rows
         for ((i, estimatesArray) in estimates.withIndex()){
             // make the bootstrap estimates
-            val originalEstimate = originalDataEstimates[i]
+            val originalEstimate = originalDataEstimate[i]
             val be = BootstrapEstimate(estimator.names[i], myOriginalPop.size(), originalEstimate, estimatesArray)
             list.add(be)
         }
