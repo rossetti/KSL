f3db3ee058238cd839d5d5efad908ba287b249ed
Manuel Rossetti <rossetti@uark.edu>
2023-12-27 16:53:24 -0600

    Update SamplingDistribution.kt

--- KSLExamples/src/main/kotlin/ksl/examples/general/montecarlo/SamplingDistribution.kt
+++ KSLExamples/src/main/kotlin/ksl/examples/general/montecarlo/SamplingDistribution.kt
@@ -4,17 +4,19 @@ import ksl.utilities.distributions.Normal
 import ksl.utilities.io.plotting.FitDistPlot
 import ksl.utilities.random.rvariable.NormalRV
 import ksl.utilities.random.rvariable.UniformRV
+import ksl.utilities.statistic.Statistic
 import ksl.utilities.statistic.averages
 import ksl.utilities.statisticsByRow
 
 
 /**
- * The purpose of this example is to illustrate the generation
+ * The purpose of these examples is to illustrate the generation
  * of a sampling distribution.
  */
 fun main(){
     normalExample()
     uniformExample()
+    confIntervalCoverage()
 }
 
 fun normalExample(){
@@ -26,7 +28,7 @@ fun normalExample(){
     val n = Normal(5.0, 4.0/10.0)
     val plot = FitDistPlot(averages, n, n)
     plot.showInBrowser()
-    plot.saveToFile("FitDistPlot")
+    plot.saveToFile("SamplingDistForNorm")
 }
 
 fun uniformExample(){
@@ -38,5 +40,18 @@ fun uniformExample(){
     val n = Normal(35.0, 20.83333)
     val plot = FitDistPlot(averages, n, n)
     plot.showInBrowser()
-    plot.saveToFile("FitDistPlot")
+    plot.saveToFile("SamplingDistForUniform")
+}
+
+fun confIntervalCoverage(){
+    val x = NormalRV(5.0, 4.0)
+    // create 1000 samples of size 10
+    val samples = x.sampleAsRows(10, 1000)
+    val stats = samples.statisticsByRow()
+    val ciStat = Statistic("Coverage Probability")
+    for(stat in stats){
+        ciStat.collect(stat.confidenceInterval.contains(5.0))
+    }
+    println(ciStat)
+
 }
