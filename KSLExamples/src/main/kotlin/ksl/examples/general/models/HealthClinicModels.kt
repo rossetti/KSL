ffa8af3f3fc031e02ed39fdb56cb5fc5ab8429ee
Manuel Rossetti <rossetti@uark.edu>
2024-03-11 12:29:56 -0500

    Update HealthClinicModels.kt

--- KSLExamples/src/main/kotlin/ksl/examples/general/models/HealthClinicModels.kt
+++ KSLExamples/src/main/kotlin/ksl/examples/general/models/HealthClinicModels.kt
@@ -1,11 +1,17 @@
 package ksl.examples.general.models
 
+import ksl.observers.ExperimentDataCollector
+import ksl.observers.ReplicationDataCollector
 import ksl.simulation.Model
+import ksl.utilities.statistic.MultipleComparisonAnalyzer
 
 fun main(){
     val modelA = Model("Clinic A Model")
     val clinicA = ClinicDesignA(modelA, "Design A")
     modelA.numberOfReplications = 10
+
+    val rdc1 = ReplicationDataCollector(modelA)
+    rdc1.addResponse(clinicA.systemTime)
     modelA.simulate()
     modelA.print()
 
@@ -13,7 +19,21 @@ fun main(){
     val clinicB = ClinicDesignB(modelB, "Design B")
     modelB.resetStartStream()
     modelB.numberOfReplications = 10
+
+    val rdc2 = ReplicationDataCollector(modelB)
+    rdc2.addResponse(clinicB.systemTime)
+
     modelB.simulate()
     modelB.print()
+
+    val dataA = rdc1.replicationData(clinicA.systemTime.name)
+    val dataB = rdc2.replicationData(clinicB.systemTime.name)
+
+    val dataMap = mutableMapOf(
+        "Design A System Time" to dataA, "Design B System Time" to dataB )
+
+    val mc = MultipleComparisonAnalyzer(dataMap)
+
+    println(mc)
 
 }
