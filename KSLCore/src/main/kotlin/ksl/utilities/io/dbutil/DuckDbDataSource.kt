6f782971cd4cbf4a54f7d118b4482e3c596a320f
Manuel Rossetti <rossetti@uark.edu>
2024-03-23 21:34:53 -0500

    start duckdb

---
+++ KSLCore/src/main/kotlin/ksl/utilities/io/dbutil/DuckDbDataSource.kt
@@ -0,0 +1,18 @@
+package ksl.utilities.io.dbutil
+
+import ksl.utilities.io.KSL
+import java.nio.file.Path
+import java.util.Properties
+
+class DuckDbDataSource(
+    pathToFile: Path,
+    properties: Properties = Properties()
+) : BaseDataSource("jdbc:duckdb:${pathToFile}", properties) {
+}
+
+fun main() {
+    val path = KSL.dbDir.resolve("TestDuckDb.db")
+    val ds = DuckDbDataSource(path)
+    val con = ds.connection
+    con.close()
+}
