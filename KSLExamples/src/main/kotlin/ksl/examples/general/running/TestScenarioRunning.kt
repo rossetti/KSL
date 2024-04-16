32b5b537c48342a810d592c05cb7bbb27a25f2a8
Manuel Rossetti <rossetti@uark.edu>
2024-04-16 18:21:30 -0500

    Update TestScenarioRunning.kt

--- KSLExamples/src/main/kotlin/ksl/examples/general/running/TestScenarioRunning.kt
+++ KSLExamples/src/main/kotlin/ksl/examples/general/running/TestScenarioRunning.kt
@@ -1,27 +1,38 @@
 package ksl.examples.general.running
 
+import ksl.controls.experiments.Scenario
 import ksl.controls.experiments.ScenarioRunner
 import ksl.simulation.Model
 
 
 fun main(){
-   // val scenarioRunner = ScenarioRunner()
-
-
+    val scenarioRunner = ScenarioRunner("ScenarioRunner", buildScenarios())
+    scenarioRunner.simulate()
+    for(s in scenarioRunner.scenarioList) {
+        val sr = s.simulationRun?.statisticalReporter()
+        val r = sr?.halfWidthSummaryReport(title = s.experimentName)
+        println(r)
+        println()
+    }
 }
 
-fun buildScenarios() {
+fun buildScenarios() : List<Scenario> {
     val sim1 = Model("MM1 Test1")
     sim1.numberOfReplications = 3
     sim1.lengthOfReplication = 100.0
     sim1.lengthOfReplicationWarmUp = 50.0
     GIGcQueue(sim1, 1, name = "MM1Q")
+    val sim1Inputs = mapOf("MM1Q.numServers" to 2.0)
 
     val sim2 = Model("MM1 Test2")
     sim2.numberOfReplications = 10
     sim2.lengthOfReplication = 1000.0
     sim2.lengthOfReplicationWarmUp = 50.0
     GIGcQueue(sim2, 1, name = "MM1Q")
+    val sim2Inputs = mapOf("MM1Q.numServers" to 3.0)
 
-    
+    val s1 = Scenario(model = sim1, inputs = sim1Inputs)
+    val s2 = Scenario(model = sim2, inputs = sim2Inputs)
+    val list = listOf(s1, s2)
+    return list
 }
