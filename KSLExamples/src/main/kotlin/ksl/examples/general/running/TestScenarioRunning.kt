8f1d9e9d45a3095e28a9d01af228f0f08e620dcc
Manuel Rossetti <rossetti@uark.edu>
2024-04-16 17:18:18 -0500

    Update TestScenarioRunning.kt

--- KSLExamples/src/main/kotlin/ksl/examples/general/running/TestScenarioRunning.kt
+++ KSLExamples/src/main/kotlin/ksl/examples/general/running/TestScenarioRunning.kt
@@ -1,8 +1,27 @@
 package ksl.examples.general.running
 
 import ksl.controls.experiments.ScenarioRunner
+import ksl.simulation.Model
+
 
 fun main(){
-    val scenarioRunner = ScenarioRunner()
+   // val scenarioRunner = ScenarioRunner()
 
+
+}
+
+fun buildScenarios() {
+    val sim1 = Model("MM1 Test1")
+    sim1.numberOfReplications = 3
+    sim1.lengthOfReplication = 100.0
+    sim1.lengthOfReplicationWarmUp = 50.0
+    GIGcQueue(sim1, 1, name = "MM1Q")
+
+    val sim2 = Model("MM1 Test2")
+    sim2.numberOfReplications = 10
+    sim2.lengthOfReplication = 1000.0
+    sim2.lengthOfReplicationWarmUp = 50.0
+    GIGcQueue(sim2, 1, name = "MM1Q")
+
+    
 }
