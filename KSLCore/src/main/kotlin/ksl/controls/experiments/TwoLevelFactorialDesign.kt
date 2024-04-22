4a6a3069bf11182d2e115633319bb69eeb267bdc
Manuel Rossetti <rossetti@uark.edu>
2024-04-22 09:47:45 -0500

    half-fraction work

--- KSLCore/src/main/kotlin/ksl/controls/experiments/TwoLevelFactorialDesign.kt
+++ KSLCore/src/main/kotlin/ksl/controls/experiments/TwoLevelFactorialDesign.kt
@@ -1,6 +1,5 @@
 package ksl.controls.experiments
 
-import ksl.utilities.KSLArrays
 import ksl.utilities.product
 
 class TwoLevelFactorialDesign(
@@ -9,43 +8,42 @@ class TwoLevelFactorialDesign(
 ) : FactorialDesign(factors, name){
 
     /**
-     *  This iterator should present each design point
-     *  until all points in the design have been presented.
+     *  @param half indicates the half-fraction to iterate. 1.0 indicates the positive
+     *  half-fraction and -1.0 the negative half-fraction. The default is 1.0
      */
-    private inner class HalfFractionIterator(val half: Double = 1.0) : DesignPointIteratorIfc {
+    fun halfFractionIterator(half: Double = 1.0) : HalfFractionIterator {
+        return HalfFractionIterator(half)
+    }
+
+    /**
+     *  This iterator should present each design point in the associated half-fraction
+     *  until all points in the half-fraction have been presented.
+     */
+    inner class HalfFractionIterator(val half: Double = 1.0) : DesignPointIteratorIfc {
+
+        // The internal iterator for the points
+        private val itr : Iterator<DesignPoint>
         init {
             require((half == 1.0) || (half == -1.0)) { "The half fraction must be 1.0 or -1.0"}
+            // make the sequence and get the iterator
+            val tmp = this@TwoLevelFactorialDesign.iterator()
+            itr = tmp.asSequence().filter { it.codedValues().product() == half }.iterator()
         }
-        // use the main iterator to go through all point
-        private val itr = iterator() as DesignPointIteratorIfc
 
-        override val count: Int
-            get() = itr.count
+        override var count: Int = 0
+            private set
 
-        override val last: DesignPoint?
-            get() = itr.last
+        override var last: DesignPoint? = null
+            private set
 
         override fun hasNext(): Boolean {
             return itr.hasNext()
         }
 
         override fun next(): DesignPoint {
-            // get the next design point
-            var next: DesignPoint = itr.next()
-//            do {
-//
-//            }
-            TODO("Not implemented yet")
-
-//            var found = false
-//            while (!found) {
-//                next = itr.next()
-//                if (next.codedValues().product() == half){
-//                    found = true
-//                }
-//            }
-//
-//            return next
+            count++
+            last = itr.next()
+            return last!!
         }
     }
 }
