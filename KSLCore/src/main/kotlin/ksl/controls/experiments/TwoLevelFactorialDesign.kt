9506e82b10dd5e95dca50db1c4490a7b6bb3431b
Manuel Rossetti <rossetti@uark.edu>
2024-04-21 19:10:05 -0500

    Update TwoLevelFactorialDesign.kt

--- KSLCore/src/main/kotlin/ksl/controls/experiments/TwoLevelFactorialDesign.kt
+++ KSLCore/src/main/kotlin/ksl/controls/experiments/TwoLevelFactorialDesign.kt
@@ -1,6 +1,7 @@
 package ksl.controls.experiments
 
 import ksl.utilities.KSLArrays
+import ksl.utilities.product
 
 class TwoLevelFactorialDesign(
     factors: Set<TwoLevelFactor>,
@@ -11,23 +12,40 @@ class TwoLevelFactorialDesign(
      *  This iterator should present each design point
      *  until all points in the design have been presented.
      */
-    private inner class HalfFractionIterator : DesignPointIteratorIfc {
-        override var count: Int = 0
-            private set
+    private inner class HalfFractionIterator(val half: Double = 1.0) : DesignPointIteratorIfc {
+        init {
+            require((half == 1.0) || (half == 0.0)) { "The half fraction must be 1.0 or -1.0"}
+        }
+        // use the main iterator to go through all point
+        private val itr = iterator() as DesignPointIteratorIfc
 
-        override var last: DesignPoint? = null
-            private set
+        override val count: Int
+            get() = itr.count
+
+        override val last: DesignPoint?
+            get() = itr.last
 
         override fun hasNext(): Boolean {
-            return count < numDesignPoints
+            return itr.hasNext()
         }
 
         override fun next(): DesignPoint {
-            count++
-            last = designPoint(count)
-          //  last!!.codedValues()
-            return last!!
-        }
+            // get the next design point
+            var next: DesignPoint = itr.next()
+//            do {
+//
+//            }
+            TODO("Not implemented yet")
 
+//            var found = false
+//            while (!found) {
+//                next = itr.next()
+//                if (next.codedValues().product() == half){
+//                    found = true
+//                }
+//            }
+//
+//            return next
+        }
     }
 }
