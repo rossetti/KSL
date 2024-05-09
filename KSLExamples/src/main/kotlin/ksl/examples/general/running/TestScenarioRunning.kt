75d275703fa2694423c7a801aba01a2f2c44faea
Manuel Rossetti <rossetti@uark.edu>
2024-05-09 13:56:08 -0500

    Update TestScenarioRunning.kt

--- KSLExamples/src/main/kotlin/ksl/examples/general/running/TestScenarioRunning.kt
+++ KSLExamples/src/main/kotlin/ksl/examples/general/running/TestScenarioRunning.kt
@@ -15,13 +15,13 @@ fun main(){
         println()
     }
 
-    scenarioRunner.simulate(clearAllData = false)
-    for(s in scenarioRunner.scenarioList) {
-        val sr = s.simulationRun?.statisticalReporter()
-        val r = sr?.halfWidthSummaryReport(title = s.experimentName)
-        println(r)
-        println()
-    }
+//    scenarioRunner.simulate(clearAllData = false)
+//    for(s in scenarioRunner.scenarioList) {
+//        val sr = s.simulationRun?.statisticalReporter()
+//        val r = sr?.halfWidthSummaryReport(title = s.experimentName)
+//        println(r)
+//        println()
+//    }
 }
 
 fun buildScenarios() : List<Scenario> {
