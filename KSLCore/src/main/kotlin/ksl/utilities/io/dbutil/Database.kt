1a650026848cb6493af96a1016adff5ee1e79f08
Manuel Rossetti <rossetti@uark.edu>
2024-06-07 11:59:10 -0500

    Allow DbTableData to define a database

--- KSLCore/src/main/kotlin/ksl/utilities/io/dbutil/Database.kt
+++ KSLCore/src/main/kotlin/ksl/utilities/io/dbutil/Database.kt
@@ -48,23 +48,7 @@ open class Database(
 //    val db = Database.connect(dataSource, logger = Slf4jLoggerAdapter(DatabaseIfc.logger))
 
     override fun toString(): String {
-        val sb = StringBuilder()
-        sb.appendLine("Database: $label")
-        sb.appendLine("The database was connected via url $dbURL")
-        sb.appendLine("The database has the following schemas:")
-        sb.append("\t")
-        sb.append(schemas.toString())
-        sb.appendLine()
-        sb.appendLine("The default schema is $defaultSchemaName")
-        sb.appendLine("The database has the following user defined tables:")
-        sb.append("\t")
-        sb.append(userDefinedTables.toString())
-        sb.appendLine()
-        sb.appendLine("The database has the following views:")
-        sb.append("\t")
-        sb.append(views.toString())
-        sb.appendLine()
-        return sb.toString()
+        return asString()
     }
 
     companion object {
---
+++ KSLCore/src/main/kotlin/ksl/utilities/io/dbutil/SimpleDb.kt
@@ -0,0 +1,54 @@
+/*
+ *     The KSL provides a discrete-event simulation library for the Kotlin programming language.
+ *     Copyright (C) 2023  Manuel D. Rossetti, rossetti@uark.edu
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ksl.utilities.io.dbutil
+
+import ksl.utilities.io.KSL
+import java.nio.file.Path
+
+/**
+ *  @param tableDefinitions an example set of table definitions based on DbTableData specifications
+ *  @param db the underlying database
+ */
+class SimpleDb(
+    tableDefinitions: Set<DbTableData>,
+    private val db: Database
+) : DatabaseIfc by db {
+
+    /** This constructs a SQLite database on disk.
+     * The database will be empty.
+     *
+     * @param tableDefinitions an example set of table definitions based on DbTableData specifications
+     * @param dbName the name of the database
+     * @param dbDirectory the directory containing the database. By default, KSL.dbDir.
+     * @return an empty embedded SQLite database
+     */
+    constructor(tableDefinitions: Set<DbTableData>, dbName: String, dbDirectory: Path = KSL.dbDir) : this(
+        tableDefinitions, SQLiteDb.createDatabase(dbName, dbDirectory))
+
+    init {
+        for (tableData in tableDefinitions) {
+            val worked = executeCommand(tableData.createTableSQLStatement())
+            if (worked){
+                DatabaseIfc.logger.info { "SimpleDb($label): table ${tableData.tableName} has been created." }
+            } else {
+                DatabaseIfc.logger.info { "SimpleDb($label): table ${tableData.tableName} was not created." }
+            }
+        }
+    }
+}
