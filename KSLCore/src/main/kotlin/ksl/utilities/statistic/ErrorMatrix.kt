a2d9ebe41babf1d80e521a87713fec3f500ab435
Manuel Rossetti <rossetti@uark.edu>
2024-06-22 06:43:50 -0500

    Update ErrorMatrix.kt

--- KSLCore/src/main/kotlin/ksl/utilities/statistic/ErrorMatrix.kt
+++ KSLCore/src/main/kotlin/ksl/utilities/statistic/ErrorMatrix.kt
@@ -2,6 +2,7 @@ package ksl.utilities.statistic
 
 import ksl.utilities.Identity
 import ksl.utilities.IdentityIfc
+import ksl.utilities.io.dbutil.DbTableData
 import ksl.utilities.io.plotting.StringFrequencyPlot
 import ksl.utilities.random.robj.DEmpiricalList
 import ksl.utilities.random.rvariable.toDouble
@@ -17,11 +18,12 @@ enum class ConfusionResult {
     TP, FP, FN, TN
 }
 
-data class ClassificationCase (val actual: Double, val predicted: Double) {
+data class ClassificationCase(val actual: Double, val predicted: Double) {
     constructor(actual: Int, predicted: Int) : this(actual.toDouble(), predicted.toDouble())
     constructor(predicted: Boolean, actual: Boolean) : this(predicted.toDouble(), actual.toDouble())
+
     val classification
-        get() = ConfusionMatrix.classify(actual,predicted)
+        get() = ErrorMatrix.classify(actual, predicted)
 }
 
 /**
@@ -30,17 +32,16 @@ data class ClassificationCase (val actual: Double, val predicted: Double) {
  *  There are two classes class 1 (positive) and class 0 (negative)
  *  An instance (exemplar) must be in one of the classes.
  */
-class ConfusionMatrix(
+class ErrorMatrix(
     data: Collection<ConfusionResult>? = null,
     name: String? = null,
 ) : IdentityIfc by Identity(name) {
 
     val stringFrequency: StringFrequency
 
-
     init {
         val limitSet = ConfusionResult.entries.map { it.toString() }.toSet()
-        stringFrequency = StringFrequency(name = name, limitSet = limitSet )
+        stringFrequency = StringFrequency(name = name, limitSet = limitSet)
         if (data != null) {
             for (result in data) {
                 stringFrequency.collect(result.toString())
@@ -52,33 +53,17 @@ class ConfusionMatrix(
      *  Present the case to the matrix for tabulation
      *  first = actual, second = predicted
      */
-    fun collect(case : ClassificationCase) {
+    fun collect(case: ClassificationCase) {
         collect(case.classification)
     }
-
-//    /**
-//     *  Present the case to the matrix for tabulation
-//     *  first = actual, second = predicted
-//     */
-//    fun collect(case : Pair<Int, Int>) {
-//        collect(classify(case.first, case.second))
-//    }
 
     /**
      *  Present the case to the matrix for tabulation
      *  first = actual, second = predicted
      */
-    fun collect(case : Pair<Boolean, Boolean>) {
+    fun collect(case: Pair<Double, Double>) {
         collect(classify(case.first, case.second))
     }
-
-//    /**
-//     *  Present the case to the matrix for tabulation
-//     *  first = actual, second = predicted
-//     */
-//    fun collect(case : Pair<Double, Double>) {
-//        collect(classify(case.first, case.second))
-//    }
 
     /**
      *  Present the case to the matrix for tabulation
@@ -155,11 +140,11 @@ class ConfusionMatrix(
         return sb.toString()
 
     }
-    
-    val numTP: Int get() = count(ConfusionResult.TP)
-    val numFP: Int get() = count(ConfusionResult.FP)
-    val numTN: Int get() = count(ConfusionResult.TN)
-    val numFN: Int get() = count(ConfusionResult.FN)
+
+    val numTP: Int get() = count(TP)
+    val numFP: Int get() = count(FP)
+    val numTN: Int get() = count(TN)
+    val numFN: Int get() = count(FN)
     val numP: Int get() = numTP + numFN
     val numN: Int get() = numFP + numTN
     val total: Int get() = stringFrequency.totalCount
@@ -273,14 +258,22 @@ class ConfusionMatrix(
             return num / dnom
         }
 
-      fun frequencyData(sortedByCount: Boolean = false) : List<StringFrequencyData> {
-          return stringFrequency.frequencyData(sortedByCount)
-      }
+    fun frequencyData(sortedByCount: Boolean = false): List<StringFrequencyData> {
+        return stringFrequency.frequencyData(sortedByCount)
+    }
 
-      fun frequencyPlot(proportions: Boolean = false): StringFrequencyPlot {
-          return stringFrequency.frequencyPlot(proportions)
-      }
+    fun frequencyPlot(proportions: Boolean = false): StringFrequencyPlot {
+        return stringFrequency.frequencyPlot(proportions)
+    }
 
+    fun asErrorMatrixData() : ErrorMatrixData {
+        return ErrorMatrixData(id = id, name = name, numTP, numFP, numTN, numFN)
+    }
+
+    fun asErrorMatrixRecord() : ErrorMatrixRecord {
+        return ErrorMatrixRecord(id = id, name = name, numTP, numFP, numTN, numFN)
+    }
+    
     companion object {
         /**
          *  actual true means that the instance belongs to class 1 (positive)
@@ -342,14 +335,87 @@ class ConfusionMatrix(
     }
 }
 
-fun main() {
-    val possibilities = listOf(ConfusionResult.TP, ConfusionResult.FP,
-        ConfusionResult.FN, ConfusionResult.TN)
-    val rList = DEmpiricalList<ConfusionResult>(possibilities, doubleArrayOf(0.20, 0.7, 0.8, 1.0 ))
-    val data = rList.sample(100)
-//    println(data.joinToString())
+data class ErrorMatrixData(
+    var id: Int = 1,
+    var name: String = "",
+    var numTP: Int = 1,
+    var numFP: Int = 1,
+    var numTN: Int = 1,
+    var numFN: Int = 1,
+    var numP: Int = numTP + numFN,
+    var numN: Int = numFP + numTN,
+    var total: Int = numP + numN,
+    var numPP: Int = numTP + numFP,
+    var numPN: Int = numFN + numTN,
+    var prevalence: Double = if (total == 0) Double.NaN else numPP / total.toDouble(),
+    var accuracy: Double = if (total == 0) Double.NaN else (numTP + numTN) / total.toDouble(),
+    var truePositiveRate: Double = if (numP == 0) Double.NaN else numTP / numP.toDouble(),
+    var falseNegativeRate: Double = if (numP == 0) Double.NaN else numFN / numP.toDouble(),
+    var falsePositiveRate: Double = if (numN == 0) Double.NaN else numFP / numN.toDouble(),
+    var trueNegativeRate: Double = if (numN == 0) Double.NaN else numTN / numN.toDouble(),
+    var falseOmissionRate: Double = if (numPN == 0) Double.NaN else numFN / numPN.toDouble(),
+    var precision: Double = if (numPP == 0) Double.NaN else numTP / numPP.toDouble(),
+    var positivePredictiveValue: Double = precision,
+    var falseDiscoveryRate: Double = if (numPP == 0) Double.NaN else numFP / numPP.toDouble(),
+    var negativePredictiveValue: Double = if (numPN == 0) Double.NaN else numTN / numPN.toDouble(),
+    var positiveLikelihoodRatio: Double = truePositiveRate / falsePositiveRate,
+    var negativeLikelihoodRatio: Double = falseNegativeRate / trueNegativeRate,
+    var markedness: Double = positivePredictiveValue + negativePredictiveValue - 1.0,
+    var diagnosticOddsRatio: Double = positiveLikelihoodRatio / negativeLikelihoodRatio,
+    var balancedAccuracy: Double = (truePositiveRate + trueNegativeRate) / 2.0,
+    var f1Score: Double = (2.0 * positivePredictiveValue * truePositiveRate) / (positivePredictiveValue + truePositiveRate),
+    var fowlkesMallowsIndex: Double = sqrt(positivePredictiveValue * truePositiveRate),
+    var mathhewsCorrelationCoefficient: Double = sqrt(truePositiveRate * trueNegativeRate * positivePredictiveValue * negativePredictiveValue)
+            - sqrt(falseNegativeRate * falsePositiveRate * falseOmissionRate * falseDiscoveryRate),
+    var threatScore: Double = if (total == 0) Double.NaN else numTP / (numTP + numFN + numFP).toDouble(),
+    var informedness: Double = truePositiveRate + trueNegativeRate - 1.0,
+    var prevalenceThreshold: Double = (sqrt(truePositiveRate * falsePositiveRate) - falsePositiveRate) /
+            (truePositiveRate - falsePositiveRate)
+)
 
-    val sf = ConfusionMatrix(data = data)
+data class ErrorMatrixRecord(
+    var id: Int = 1,
+    var name: String = "",
+    var numTP: Int = 1,
+    var numFP: Int = 1,
+    var numTN: Int = 1,
+    var numFN: Int = 1,
+    var numP: Int = numTP + numFN,
+    var numN: Int = numFP + numTN,
+    var total: Int = numP + numN,
+    var numPP: Int = numTP + numFP,
+    var numPN: Int = numFN + numTN,
+    var prevalence: Double = if (total == 0) Double.NaN else numPP / total.toDouble(),
+    var accuracy: Double = if (total == 0) Double.NaN else (numTP + numTN) / total.toDouble(),
+    var truePositiveRate: Double = if (numP == 0) Double.NaN else numTP / numP.toDouble(),
+    var falseNegativeRate: Double = if (numP == 0) Double.NaN else numFN / numP.toDouble(),
+    var falsePositiveRate: Double = if (numN == 0) Double.NaN else numFP / numN.toDouble(),
+    var trueNegativeRate: Double = if (numN == 0) Double.NaN else numTN / numN.toDouble(),
+    var falseOmissionRate: Double = if (numPN == 0) Double.NaN else numFN / numPN.toDouble(),
+    var precision: Double = if (numPP == 0) Double.NaN else numTP / numPP.toDouble(),
+    var positivePredictiveValue: Double = precision,
+    var falseDiscoveryRate: Double = if (numPP == 0) Double.NaN else numFP / numPP.toDouble(),
+    var negativePredictiveValue: Double = if (numPN == 0) Double.NaN else numTN / numPN.toDouble(),
+    var positiveLikelihoodRatio: Double = truePositiveRate / falsePositiveRate,
+    var negativeLikelihoodRatio: Double = falseNegativeRate / trueNegativeRate,
+    var markedness: Double = positivePredictiveValue + negativePredictiveValue - 1.0,
+    var diagnosticOddsRatio: Double = positiveLikelihoodRatio / negativeLikelihoodRatio,
+    var balancedAccuracy: Double = (truePositiveRate + trueNegativeRate) / 2.0,
+    var f1Score: Double = (2.0 * positivePredictiveValue * truePositiveRate) / (positivePredictiveValue + truePositiveRate),
+    var fowlkesMallowsIndex: Double = sqrt(positivePredictiveValue * truePositiveRate),
+    var mathhewsCorrelationCoefficient: Double = sqrt(truePositiveRate * trueNegativeRate * positivePredictiveValue * negativePredictiveValue)
+            - sqrt(falseNegativeRate * falsePositiveRate * falseOmissionRate * falseDiscoveryRate),
+    var threatScore: Double = if (total == 0) Double.NaN else numTP / (numTP + numFN + numFP).toDouble(),
+    var informedness: Double = truePositiveRate + trueNegativeRate - 1.0,
+    var prevalenceThreshold: Double = (sqrt(truePositiveRate * falsePositiveRate) - falsePositiveRate) /
+            (truePositiveRate - falsePositiveRate)
+): DbTableData("tblErrorMatrix", listOf("id"))
+
+fun main() {
+    val possibilities = listOf(TP, FP, FN, TN)
+    val rList = DEmpiricalList<ConfusionResult>(possibilities, doubleArrayOf(0.20, 0.7, 0.8, 1.0))
+    val data = rList.sample(100)
+    val sf = ErrorMatrix(data = data)
     println(sf)
     sf.frequencyPlot().showInBrowser()
     println(sf.frequencyData().toDataFrame())
