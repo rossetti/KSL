499ecbed55799778f85e25ef12942e4853cfe04a
Manuel Rossetti <rossetti@uark.edu>
2024-07-22 17:42:38 -0500

    Update SResource.kt

--- KSLCore/src/main/kotlin/ksl/modeling/station/SResource.kt
+++ KSLCore/src/main/kotlin/ksl/modeling/station/SResource.kt
@@ -2,6 +2,7 @@ package ksl.modeling.station
 
 import ksl.modeling.variable.TWResponse
 import ksl.modeling.variable.TWResponseCIfc
+import ksl.simulation.Model
 import ksl.simulation.ModelElement
 
 /**
@@ -31,6 +32,9 @@ class SResource(
     var initialCapacity = capacity
         set(value) {
             require(value >= 1) { "The initial capacity of the resource must be >= 1" }
+            if (model.isRunning) {
+                Model.logger.warn { "Changed the initial capacity of $name during replication ${model.currentReplicationNumber}." }
+            }
             field = value
         }
 
@@ -49,10 +53,53 @@ class SResource(
     /**
      * Counts how many times the resource has units become idle
      */
-    var myNumTimesReleased = 0
+    var numTimesReleased = 0
         private set
 
-    private val myNumBusy: TWResponse = TWResponse(this, "${this}:NumBusy")
-    val numBusy: TWResponseCIfc
+    private val myNumBusy = TWResponse(this, "${this.name}:NumBusyUnits")
+    val numBusyUnits: TWResponseCIfc
         get() = myNumBusy
+
+    val numAvailableUnits: Int
+        get() = capacity - myNumBusy.value.toInt()
+
+    val hasAvailableUnits: Boolean
+        get() = numAvailableUnits > 0
+
+    /** Checks if the resource is idle, has no units allocated
+     */
+    val isIdle: Boolean
+        get() = myNumBusy.value == 0.0
+
+    /** Checks to see if the resource is busy, has some units allocated
+     */
+    val isBusy: Boolean
+        get() = myNumBusy.value > 0.0
+
+    override fun initialize() {
+        capacity = initialCapacity
+        numTimesReleased = 0
+        numTimesSeized = 0
+    }
+
+    /**
+     * Seizes amt units of the resource. If amt = 0, then an exception occurs. If
+     * the resource has no units available, then an exception occurs. If the amt
+     * &gt; getNumberAvailable() then an exception occurs.
+     *
+     * @param amt the amount to seize
+     */
+    fun seize(amt: Int = 1) {
+        require(amt > 0) { "The seize amount must be > 0" }
+        if (!hasAvailableUnits) {
+            val s = "Tried to increment number busy with no units available"
+            throw IllegalStateException(s)
+        }
+        if (amt > numAvailableUnits) {
+            val s = "Tried to increment number busy by $amt with $numAvailableUnits units available"
+            throw IllegalStateException(s)
+        }
+        myNumBusy.increment(amt.toDouble())
+        numTimesSeized++
+    }
 }
