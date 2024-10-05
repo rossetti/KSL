13a034ba2d3bcec88d35179e2a89370a06319f4c
Manuel Rossetti <rossetti@uark.edu>
2024-10-05 10:55:34 -0500

    demo mcb

--- KSLCore/src/main/kotlin/ksl/utilities/statistic/MultipleComparisonAnalyzer.kt
+++ KSLCore/src/main/kotlin/ksl/utilities/statistic/MultipleComparisonAnalyzer.kt
@@ -3914,7 +3914,7 @@ class MultipleComparisonAnalyzer(
 }
 
 fun main() {
-    val data = LinkedHashMap<String, DoubleArray>()
+    val data = mutableMapOf<String, DoubleArray>()
     val d1 = doubleArrayOf(63.72, 32.24, 40.28, 36.94, 36.29, 56.94, 34.10, 63.36, 49.29, 87.20)
     val d2 = doubleArrayOf(63.06, 31.78, 40.32, 37.71, 36.79, 57.93, 33.39, 62.92, 47.67, 80.79)
     val d3 = doubleArrayOf(57.74, 29.65, 36.52, 35.71, 33.81, 51.54, 31.39, 57.24, 42.63, 67.27)
