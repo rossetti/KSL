419194fe6513e60b3561264cba17941ad80c133a
Manuel Rossetti <rossetti@uark.edu>
2024-10-15 15:23:20 -0400

    Update PWCEmpiricalRV.kt

--- KSLCore/src/main/kotlin/ksl/utilities/random/rvariable/PWCEmpiricalRV.kt
+++ KSLCore/src/main/kotlin/ksl/utilities/random/rvariable/PWCEmpiricalRV.kt
@@ -9,18 +9,19 @@ import ksl.utilities.random.rng.RNStreamIfc
  *  A piecewise linear approximation forms the basis for the CDF where the breakpoints
  *  form the linear segments. There must be at least 1 interval (and two breakpoints).
  *
- *  @param proportions A double array holding the proportion associated with the intervals defined
- *  by the breakpoints. All proportions must be strictly greater than 0 and strictly
- *  less than 1.
  *  @param breakPoints The break points defining the intervals such that p[j] is
  *  associated with breakpoints b[j] and b[j+1] for j = 0, 1,..., n-1, where n
  *  is the number of break points. The number of breakpoints should be 1 more than
  *  the number of proportions. The breakpoints must be strictly increasing.
+ *  @param proportions A double array holding the proportion associated with the intervals defined
+ *  by the breakpoints. All proportions must be strictly greater than 0 and strictly
+ *  less than 1. If not supplied, the default is intervals with equal probability. That is,
+ *  with proportion equal to 1.0/(n - 1).
  *  @param stream The random number stream.
  */
 class PWCEmpiricalRV(
-    proportions: DoubleArray,
     breakPoints: DoubleArray,
+    proportions: DoubleArray = DoubleArray(breakPoints.size - 1) { 1.0 / (breakPoints.size - 1) },
     stream: RNStreamIfc = KSLRandom.nextRNStream()
 ) : RVariable(stream) {
     private val myProportions: DoubleArray
@@ -28,15 +29,19 @@ class PWCEmpiricalRV(
     private val myCDFPoints: DoubleArray
     private val mySlopes: DoubleArray
 
-    constructor(proportions: DoubleArray, breakPoints: DoubleArray, streamNum: Int) : this(
-        proportions,
-        breakPoints,
-        KSLRandom.rnStream(streamNum)
+    /**
+     *  @param streamNum supplied the number of the stream instead of the stream
+     */
+    constructor(
+        breakPoints: DoubleArray,
+        proportions: DoubleArray = DoubleArray(breakPoints.size - 1) { 1.0 / (breakPoints.size - 1) },
+        streamNum: Int) : this(
+        breakPoints, proportions, KSLRandom.rnStream(streamNum)
     )
 
     init {
-        require(proportions.isNotEmpty()) {"There must be at least 1 interval"}
-        require( breakPoints.size >= 2) {"There must be at least 2 break points"}
+        require(proportions.isNotEmpty()) { "There must be at least 1 interval" }
+        require(breakPoints.size >= 2) { "There must be at least 2 break points" }
         require(proportions.size == (breakPoints.size - 1)) {
             "Improper array sizes: proportions.size = " +
                     "${proportions.size} should be 1 less than the number of breakpoints, and breakPoints.size = ${breakPoints.size} "
@@ -44,10 +49,10 @@ class PWCEmpiricalRV(
         // breakpoints must be strictly increasing
         require(breakPoints.isStrictlyIncreasing()) { "The break points must be strictly increasing" }
         for (i in proportions.indices) {
-            require(0.0 < proportions[i]) {" proportion[$i] is <= 0.0" }
-            require(proportions[i] < 1.0) {" proportion[$i] is >= 1.0" }
+            require(0.0 < proportions[i]) { " proportion[$i] is <= 0.0" }
+            require(proportions[i] < 1.0) { " proportion[$i] is >= 1.0" }
         }
-        require(KSLRandom.isValidPMF(proportions)) {"The proportions do not represent a valid probability distribution"}
+        require(KSLRandom.isValidPMF(proportions)) { "The proportions do not represent a valid probability distribution" }
         // number of intervals/points
         val n = proportions.size
         // use 1-based indexing, 1 = 1st interval
