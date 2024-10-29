6fa9f91541ce92675e4fed784c0d14bca8dc770f
Manuel Rossetti <rossetti@uark.edu>
2024-10-29 14:49:16 -0500

    Update LogisticFunction.kt

--- KSLCore/src/main/kotlin/ksl/utilities/moda/LogisticFunction.kt
+++ KSLCore/src/main/kotlin/ksl/utilities/moda/LogisticFunction.kt
@@ -12,7 +12,7 @@ class LogisticFunction(
 ) : ValueFunctionIfc {
 
     init {
-        require(scale > 0.0) {"The scale must be > 0.0"}
+        require(scale > 0.0) { "The scale must be > 0.0" }
     }
 
     /**
@@ -24,17 +24,17 @@ class LogisticFunction(
      *  Estimates the location and scale from the data
      *  @param data there must be at least two observations
      */
-    constructor(data: DoubleArray): this(estimateLocationAndScale(data))
+    constructor(data: DoubleArray) : this(estimateLocationAndScale(data))
 
     var scale: Double = scale
         set(value) {
-            require(scale > 0.0) {"The scale must be > 0.0"}
+            require(scale > 0.0) { "The scale must be > 0.0" }
             field = value
         }
 
     override fun value(score: Score): Double {
-        val z = (score.value - location)/ scale
-        var y = 1.0/(1.0 + exp(-z))
+        val z = (score.value - location) / scale
+        var y = 1.0 / (1.0 + exp(-z))
         if (score.metric.direction == MetricIfc.Direction.SmallerIsBetter) {
             y = 1.0 - y
         }
@@ -47,10 +47,13 @@ class LogisticFunction(
          *  Estimates the location and scale based on the data
          *  The location is estimated by the median.
          *  The scale is estimated using the interquartile range.
+         *  Wan, Xiang, Wenqian Wang, Jiming Liu, and Tiejun Tong. “Estimating the Sample Mean and Standard Deviation
+         *  from the Sample Size, Median, Range and/or Interquartile Range.” BMC Medical Research Methodology 14, no. 1
+         *  (December 2014): 135. https://doi.org/10.1186/1471-2288-14-135.
          *  @return the pair(location, scale)
          */
         fun estimateLocationAndScale(data: DoubleArray): Pair<Double, Double> {
-            require(data.size >= 2){"There must be at least two observations"}
+            require(data.size >= 2) { "There must be at least two observations" }
             val b = BoxPlotSummary(data)
             val sd = Statistic.estimateStdDevFromIQR(b.interQuartileRange, b.count)
             val scale = sd * sqrt(3.0) / PI
