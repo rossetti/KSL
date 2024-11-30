60ae507720bc702f188274ce4fd76b6608bc9ee0
Manuel Rossetti <rossetti@uark.edu>
2024-11-30 10:43:18 -0600

    Update TaskProcessingSystem.kt

--- KSLCore/src/main/kotlin/ksl/modeling/entity/TaskProcessingSystem.kt
+++ KSLCore/src/main/kotlin/ksl/modeling/entity/TaskProcessingSystem.kt
@@ -398,16 +398,6 @@ open class TaskProcessingSystem(
         fun activateProcessor(taskProvider: TaskDispatcher)
 
         /**
-         *  Causes the task processor to be activated and to start processing tasks from the supplied
-         *  task provider. The task provider must not be shutdown and must be idle in order to be
-         *  activated. The task processor will continue to execute tasks from the provider as long
-         *  as the provider can supply them.
-         *
-         * @param taskProvider the task provider from which tasks will be pulled after activation
-         */
-        fun activateProcessor(taskProvider: TaskDispatcher)
-
-        /**
          *  Receives the task for processing. Enqueues the task and if the
          *  processor is idle, activates the processor to process tasks.
          *
@@ -767,27 +757,6 @@ open class TaskProcessingSystem(
                 myProcessor = Processor("Processor_${this.name}")
                 activate(myProcessor!!.taskProcessing)
             }
-        }
-
-        /**
-         *  Causes the task processor to be activated and to start processing tasks from the supplied
-         *  task provider. The task provider must not be shutdown and must be idle in order to be
-         *  activated. The task processor will continue to execute tasks from the provider as long
-         *  as the provider can supply them.
-         *
-         * @param taskProvider the task provider from which tasks will be pulled after activation
-         */
-        override fun activateProcessor(taskProvider: TaskDispatcher) {
-            require(!shutdown) { "${this.name} Task Processor: cannot be activated because it is shutdown!" }
-            require(isIdle()) { "${this.name} Task Processor: cannot be activated because it is not idle!" }
-            // must be idle thus it can be activated
-            // if the incoming task provider is different from the current provider
-            // then we can exchange it, otherwise it stays the same
-//            if (myDispatcher != taskProvider) {
-//                myDispatcher = taskProvider
-//            }
-            myProcessor = Processor("Processor_${this.name}")
-            activate(myProcessor!!.taskProcessing)
         }
 
         /**
