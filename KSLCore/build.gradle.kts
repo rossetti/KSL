1184d4ad2443bcb66d5a8745bdcca62728b31899
Manuel Rossetti <rossetti@uark.edu>
2025-01-06 09:57:30 -0600

    Update build.gradle.kts

--- KSLCore/build.gradle.kts
+++ KSLCore/build.gradle.kts
@@ -30,7 +30,7 @@ plugins {
     id("org.jetbrains.dokka") version "1.9.20"
 }
 group = "io.github.rossetti"
-version = "R1.1.6"
+version = "R1.1.7"
 
 repositories {
 
@@ -182,7 +182,7 @@ publishing {
             groupId = "io.github.rossetti"
             artifactId = "KSLCore"
             // update this field when generating new release
-            version = "R1.1.6"
+            version = "R1.1.7"
             from(components["java"])
             versionMapping {
                 usage("java-api") {
