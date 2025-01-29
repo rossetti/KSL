8c7f982bdfb61c4fd1fc8ba0991933be77aff4f4
Manuel Rossetti <rossetti@uark.edu>
2025-01-29 13:05:25 -0600

    testing

--- KSLExamples/src/main/kotlin/ksl/examples/book/chapter8/Ch8Example4.kt
+++ KSLExamples/src/main/kotlin/ksl/examples/book/chapter8/Ch8Example4.kt
@@ -3,12 +3,14 @@ package ksl.examples.book.chapter8
 import ksl.simulation.Model
 import ksl.utilities.io.KSL
 import ksl.utilities.io.MarkDown
+import ksl.utilities.io.dbutil.KSLDatabaseObserver
 
 fun main() {
     val m = Model()
     val tq = TestAndRepairShopResourceConstrained(m, name = "ResourceConstrainedTestAndRepair")
     m.numberOfReplications = 10
     m.lengthOfReplication = 52.0* 5.0*2.0*480.0
+    val kslDatabaseObserver = KSLDatabaseObserver(m)
 //    m.numberOfReplications = 1
 //    m.lengthOfReplication = 2.0*480.0
     m.simulate()
