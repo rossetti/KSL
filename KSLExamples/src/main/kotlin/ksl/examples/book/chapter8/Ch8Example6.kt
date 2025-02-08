0c65359f223029b0300e7ca4106b382bdd0d2fc6
Manuel Rossetti <rossetti@uark.edu>
2025-02-08 16:47:19 -0600

    ch8 conveyor work

--- KSLExamples/src/main/kotlin/ksl/examples/book/chapter8/Ch8Example6.kt
+++ KSLExamples/src/main/kotlin/ksl/examples/book/chapter8/Ch8Example6.kt
@@ -1,15 +1,54 @@
 package ksl.examples.book.chapter8
 
+import ksl.modeling.entity.Conveyor
 import ksl.simulation.Model
 import ksl.utilities.io.KSL
 import ksl.utilities.io.MarkDown
 
 fun main() {
+   // conveyorViaDelays()
+//    tandemQViaConveyors(Conveyor.Type.NON_ACCUMULATING)
+//    tandemQViaConveyors(Conveyor.Type.ACCUMULATING)
+    tandemQWithWorkOnConveyor(Conveyor.Type.NON_ACCUMULATING)
+//    tandemQWithWorkOnConveyor(Conveyor.Type.ACCUMULATING)
+
+}
+
+fun conveyorViaDelays() {
     val m = Model()
-    val tq = TandemQueueWithConveyors(m, name = "TandemQueueWithConveyor")
+    val tq = TandemQueueWithConveyorsViaDelay(m, name = "TandemQueueWithConveyor")
     m.numberOfReplications = 30
     m.lengthOfReplication = 20000.0
     m.lengthOfReplicationWarmUp = 5000.0
     m.simulate()
     m.print()
+    val r = m.simulationReporter
+    val out = m.outputDirectory.createPrintWriter("Conveyor via Delay")
+    r.writeHalfWidthSummaryReportAsMarkDown(out, df = MarkDown.D3FORMAT)
+}
+
+fun tandemQViaConveyors(conveyorType: Conveyor.Type) {
+    val m = Model()
+    val tq = TandemQueueWithConveyors(m, conveyorType, name = "TandemQueueWithConveyor")
+    m.numberOfReplications = 30
+    m.lengthOfReplication = 20000.0
+    m.lengthOfReplicationWarmUp = 5000.0
+    m.simulate()
+    m.print()
+    val r = m.simulationReporter
+    val out = m.outputDirectory.createPrintWriter("WorkNotOnConveyor_$conveyorType")
+    r.writeHalfWidthSummaryReportAsMarkDown(out, df = MarkDown.D3FORMAT)
+}
+
+fun tandemQWithWorkOnConveyor(conveyorType: Conveyor.Type){
+    val m = Model()
+    val tq = TandemQueueWithWorkOnConveyors(m, conveyorType, name = "TandemQueueWithConveyor")
+    m.numberOfReplications = 30
+    m.lengthOfReplication = 20000.0
+    m.lengthOfReplicationWarmUp = 5000.0
+    m.simulate()
+    m.print()
+    val r = m.simulationReporter
+    val out = m.outputDirectory.createPrintWriter("WorkOnConveyor_$conveyorType")
+    r.writeHalfWidthSummaryReportAsMarkDown(out, df = MarkDown.D3FORMAT)
 }
