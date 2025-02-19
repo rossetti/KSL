ea9d53feb349199be401424e720ef56bf1336c7e
Manuel Rossetti <rossetti@uark.edu>
2025-02-19 13:38:01 -0600

    Update Ch8Example6.kt

--- KSLExamples/src/main/kotlin/ksl/examples/book/chapter8/Ch8Example6.kt
+++ KSLExamples/src/main/kotlin/ksl/examples/book/chapter8/Ch8Example6.kt
@@ -6,9 +6,9 @@ import ksl.utilities.io.KSL
 import ksl.utilities.io.MarkDown
 
 fun main() {
-//    conveyorViaDelays()
+    conveyorViaDelays()
 //    tandemQViaConveyors(Conveyor.Type.NON_ACCUMULATING)
-    tandemQViaConveyors(Conveyor.Type.ACCUMULATING)
+//    tandemQViaConveyors(Conveyor.Type.ACCUMULATING)
 //    tandemQWithWorkOnConveyor(Conveyor.Type.NON_ACCUMULATING)
 //    tandemQWithWorkOnConveyor(Conveyor.Type.ACCUMULATING)
 
