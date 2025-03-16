81f20d2beeedc589eeb3ac8358aace07d4949003
Manuel Rossetti <rossetti@uark.edu>
2025-03-16 17:17:24 -0500

    updated KSLProjectTemplate

--- KSLExamples/build.gradle.kts
+++ KSLExamples/build.gradle.kts
@@ -16,8 +16,6 @@
  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 // An example gradle build file for a project that depends on the JSL
 
 plugins {
--- KSLProjectTemplate/build.gradle.kts
+++ KSLProjectTemplate/build.gradle.kts
@@ -1,11 +1,11 @@
 
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 // An example gradle build file for a project that depends on the KSL
 
 plugins {
     `java-library`
-    kotlin("jvm") version "1.9.20"
+//    kotlin("jvm") version "1.9.20"
+
+    kotlin("jvm") version "2.1.10"
 }
 
 repositories {
@@ -19,29 +19,32 @@ dependencies {
     // update the release number when new releases become available
     api(group = "io.github.rossetti", name = "KSLCore", version = "R1.2.0")
 
-
     testImplementation(kotlin("test"))
-    implementation(kotlin("stdlib-jdk8"))
+//    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "17"
-}
+//tasks.withType<KotlinCompile>() {
+//    kotlinOptions.jvmTarget = "17"
+//}
 
 tasks.jar {
     exclude("logback.xml")
 }
 
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "17"
+kotlin {
+    jvmToolchain(17)
 }
 
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "17"
-}
+//val compileKotlin: KotlinCompile by tasks
+//compileKotlin.kotlinOptions {
+//    jvmTarget = "17"
+//}
+//
+//val compileTestKotlin: KotlinCompile by tasks
+//compileTestKotlin.kotlinOptions {
+//    jvmTarget = "17"
+//}
