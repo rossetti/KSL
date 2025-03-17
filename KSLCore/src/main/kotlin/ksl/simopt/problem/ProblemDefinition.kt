04a59944e83b999809e42623c640ebb0572b0626
Manuel Rossetti <rossetti@uark.edu>
2025-03-17 16:23:39 -0500

    Update ProblemDefinition.kt

--- KSLCore/src/main/kotlin/ksl/simopt/problem/ProblemDefinition.kt
+++ KSLCore/src/main/kotlin/ksl/simopt/problem/ProblemDefinition.kt
@@ -15,18 +15,31 @@ enum class InequalityType {
 }
 
 
-
 class ProblemDefinition(
     val objFnResponseName: String,
     val inputNames: Set<String>,
     val responseNames: Set<String>
 ) {
 
+    private val inputs = mutableMapOf<String, InputDefinition>()
+
     var maxSamplesPerMember = 1E5.toInt()
         set(value) {
-            require (value > 0) {"The maximum number of samples per member is $value, must be > 0"}
+            require(value > 0) { "The maximum number of samples per member is $value, must be > 0" }
             field = value
         }
 
+
+    fun input(
+        name: String,
+        lowerBound: Double,
+        upperBound: Double,
+        granularity: Double = 0.0
+    ): InputDefinition {
+        require(name in inputNames) { "The name $name does not exist in the inputs" }
+        val inputData = InputDefinition(name, lowerBound, upperBound, granularity)
+        inputs[name] = inputData
+        return inputData
+    }
 
 }
