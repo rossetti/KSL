225176547a263395cb1de4b2675dff3f5f717736
Manuel Rossetti <rossetti@uark.edu>
2025-03-18 11:02:18 -0500

    Update ProblemDefinition.kt

--- KSLCore/src/main/kotlin/ksl/simopt/problem/ProblemDefinition.kt
+++ KSLCore/src/main/kotlin/ksl/simopt/problem/ProblemDefinition.kt
@@ -18,10 +18,29 @@ enum class InequalityType {
 class ProblemDefinition(
     val objFnResponseName: String,
     val inputNames: Set<String>,
-    val responseNames: Set<String>
+    val responseNames: Set<String> = emptySet()
 ) {
 
-    private val inputs = mutableMapOf<String, InputDefinition>()
+    init {
+        require(objFnResponseName.isNotBlank()) { "The objective function response name cannot be blank" }
+        require(inputNames.isNotEmpty()) { "The set of input names cannot be empty" }
+        for (name: String in inputNames) {
+            require(name.isNotBlank()) { "An input name was blank" }
+        }
+        for (name: String in responseNames) {
+            require(name.isNotBlank()) { "A response name was blank" }
+        }
+    }
+
+    private val myInputs = mutableMapOf<String, InputDefinition>()
+    val inputs: List<InputDefinition>
+        get() = myInputs.values.toList()
+    private val myInputConstraints = mutableListOf<InputConstraint>()
+    val inputConstraints: List<InputConstraint>
+        get() = myInputConstraints.toList()
+    private val myResponseConstraints = mutableListOf<ResponseConstraint>()
+    val responseConstraints: List<ResponseConstraint>
+        get() = myResponseConstraints.toList()
 
     var maxSamplesPerMember = 1E5.toInt()
         set(value) {
@@ -29,17 +48,42 @@ class ProblemDefinition(
             field = value
         }
 
-
-    fun input(
-        name: String,
-        lowerBound: Double,
-        upperBound: Double,
-        granularity: Double = 0.0
-    ): InputDefinition {
-        require(name in inputNames) { "The name $name does not exist in the inputs" }
+    fun input(name: String, lowerBound: Double, upperBound: Double, granularity: Double = 0.0): InputDefinition {
+        require(name in inputNames) { "The name $name does not exist in the named inputs" }
         val inputData = InputDefinition(name, lowerBound, upperBound, granularity)
-        inputs[name] = inputData
+        myInputs[name] = inputData
         return inputData
+    }
+
+    fun input(name: String, interval: Interval, granularity: Double = 0.0): InputDefinition {
+        return input(name, interval.lowerLimit, interval.upperLimit, granularity)
+    }
+
+    fun inputConstraint(
+        equation: Map<String, Double>,
+        rhsValue: Double = 0.0,
+        inequalityType: InequalityType = InequalityType.LESS_THAN
+    ): InputConstraint {
+        for ((name, value) in equation) {
+            require(name in inputNames) { "The name $name does not exist in the named inputs" }
+        }
+        val ic = InputConstraint(equation, rhsValue, inequalityType)
+        myInputConstraints.add(ic)
+        return ic
+    }
+
+    fun responseConstraint(
+        name: String,
+        rhsValue: Double,
+        inequalityType: InequalityType = InequalityType.LESS_THAN,
+        violationPenalty: Double = 1000.0,
+        violationExponent: Double = 2.0
+    ): ResponseConstraint {
+        require(responseNames.isNotEmpty()) { "There were no supplied response names because the set was empty" }
+        require(name in responseNames) { "The name $name does not exist in the response names" }
+        val rc = ResponseConstraint(name, rhsValue, inequalityType, violationPenalty, violationExponent)
+        myResponseConstraints.add(rc)
+        return rc
     }
 
 }
