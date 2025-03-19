13c09526411e275b4187b1ba0f2dfc034a4a768f
Manuel Rossetti <rossetti@uark.edu>
2025-03-19 15:15:14 -0500

    Update ResponseConstraint.kt

--- KSLCore/src/main/kotlin/ksl/simopt/problem/ResponseConstraint.kt
+++ KSLCore/src/main/kotlin/ksl/simopt/problem/ResponseConstraint.kt
@@ -2,18 +2,30 @@ package ksl.simopt.problem
 
 import kotlin.math.pow
 
-data class ResponseConstraint(
+class ResponseConstraint(
     val name: String,
-    val rhsValue: Double,
+    var rhsValue: Double,
     val inequalityType: InequalityType = InequalityType.LESS_THAN,
-    val violationPenalty: Double = 1000.0,
-    val violationExponent: Double = 2.0
+    violationPenalty: Double = 1000.0,
+    violationExponent: Double = 2.0
 ) {
     init {
         require(violationPenalty >= 0.0) { "The violationPenalty must be >= 0.0" }
         require(violationExponent >= 1.0) { "The violationExponent must be >= 1.0" }
         require(name.isNotBlank()) { "The response name cannot be blank" }
     }
+
+    var violationPenalty: Double = violationPenalty
+        set(value) {
+            require(value >= 0.0) { "The violationPenalty must be >= 0.0" }
+            field = value
+        }
+
+    var violationExponent: Double = violationExponent
+        set(value) {
+            require(value >= 1.0) { "The violationExponent must be >= 0.0" }
+            field = value
+        }
 
     private val inequalityFactor: Double = if (inequalityType == InequalityType.LESS_THAN) 1.0 else -1.0
 
