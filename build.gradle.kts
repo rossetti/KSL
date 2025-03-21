55ecd33b5541b27d9abd4be6c26684acc6781e49
Manuel Rossetti <rossetti@uark.edu>
2025-03-21 16:11:42 -0500

    some compile issue is occurring with K2

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     `java-library`
     // uncomment for publishing task
-    kotlin("jvm") version "1.9.20"
+    kotlin("jvm") version "2.1.10"
     kotlin("plugin.serialization") version "1.9.0"
     //   id("org.jetbrains.kotlinx.dataframe") version "0.11.0"
-    id("org.jetbrains.dokka") version "1.9.20"
+    id("org.jetbrains.dokka") version "2.0.0"
 }
 
 group = "io.github.rossetti"
-version = "R1.1.3"
+version = "R1.2.0"
 
 buildscript {
 
