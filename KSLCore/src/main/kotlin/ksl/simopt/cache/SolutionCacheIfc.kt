7165562aa70467ad202c120914a3e1684a96e5bd
Manuel Rossetti <rossetti@uark.edu>
2025-03-26 13:54:58 -0500

    continuing stubbing

---
+++ KSLCore/src/main/kotlin/ksl/simopt/cache/SolutionCacheIfc.kt
@@ -0,0 +1,29 @@
+package ksl.simopt.cache
+
+import ksl.simopt.evaluator.Solution
+import ksl.simopt.problem.InputMap
+import java.util.function.BiConsumer
+
+//TODO needs revision
+
+interface SolutionCacheIfc {
+    fun get(key: InputMap): Solution?
+
+    fun put(key: InputMap, value: Solution): Solution
+
+    fun clear()
+
+    fun keySet(): Set<InputMap>
+
+    fun values(): Collection<Solution>
+
+    fun isEmpty(): Boolean
+
+    fun containsKey(key: InputMap): Boolean
+
+    fun remove(key: InputMap): Solution?
+
+    fun forEach(action: BiConsumer<in InputMap, in Solution>)
+
+    fun size(): Int
+}
--- KSLCore/src/main/kotlin/ksl/simopt/evaluator/Evaluator.kt
+++ KSLCore/src/main/kotlin/ksl/simopt/evaluator/Evaluator.kt
@@ -1,14 +1,20 @@
 package ksl.simopt.evaluator
 
+import ksl.simopt.cache.SolutionCacheIfc
 import ksl.simopt.problem.ProblemDefinition
+import java.util.function.Consumer
 
 abstract class Evaluator(
-    val problemDefinition: ProblemDefinition
+    val problemDefinition: ProblemDefinition,
+    val cache: SolutionCacheIfc? = null
 ) {
 
     // the budget (in terms of number of evaluations)
-    var budgetReplications: Int = Int.MAX_VALUE
-        protected set
+    var maxReplicationBudget: Int = Int.MAX_VALUE
+        set(value){
+            require(value >= 1) {"The number of budgeted replications must be >= 1"}
+            field = value
+        }
     // the total number of batches evaluated
     var numBatches: Int = 0
         protected set
@@ -34,6 +40,9 @@ abstract class Evaluator(
     var numCachedReplications: Int = 0
         protected set
 
+    val hasRemainingReplications: Boolean
+        get() = numDirectReplications < maxReplicationBudget
+
     fun resetStatistics() {
         numBatches = 0
         numRequests = 0
@@ -45,4 +54,62 @@ abstract class Evaluator(
         numCachedReplications = 0
     }
 
+    /**
+     * Required method in Evaluator subclasses to evaluate responses from list of requests.
+     *
+     * @param requests - a list of requests to evaluate
+     * @return - a map containing responses for each request evaluated
+     */
+    protected abstract fun evaluateResponses(requests: List<EvaluationRequest>) : Map<EvaluationRequest, ResponseMap>
+
+    fun evaluate(requests: List<EvaluationRequest>) : List<Solution> {
+        TODO("Not implemented yet")
+    }
+
+    /**
+     * Make sure we do NOT evaluate duplicate inputs within the batch
+     * where multiple requests for the same design vector are made but for different
+     * numbers of replications the larger replication number is chosen.
+     * @param requests a list of evaluation requests
+     * @return a list of evaluation requests that are unique
+     */
+    private fun findUniqueRequests(requests: List<EvaluationRequest>): List<EvaluationRequest> {
+        TODO("Not implemented yet")
+    }
+
+    /**
+     * Evaluate directly (without accessing the cache).
+     *
+     * @param requests  list of requests (design vector input and replications)
+     * @return a map of evaluation requests with their accompanying solution
+     */
+    private fun evaluateWithNoCache(requests: List<EvaluationRequest>): Map<EvaluationRequest, Solution> {
+        TODO("Not implemented yet")
+    }
+
+    /**
+     * Match solutions to raw responses by key, parse and merge
+     * @param requests - evaluation requests
+     * @param rawResponses - a map of raw responses (Key, Value) pairs to decode, keycode
+     * is the hashcode of the solution that was evaluated.
+     * @return - solution and responses matched and parsed
+     */
+    private fun parseResponses(
+        requests: List<EvaluationRequest>,
+        rawResponses: Map<EvaluationRequest, ResponseMap>
+    ): Map<EvaluationRequest, Solution> {
+        TODO("Not implemented yet")
+    }
+
+    /**
+     * Parse an input vector and ResponseMap into a Solution
+     * throwing an error if the objective function or a required response
+     * is NOT found
+     * @param request a request for evaluation
+     * @param responseMap - a map of raw responses (the key is a hashcode of the input/inputs)
+     * @return a parsed response as a solution
+     */
+    private fun parseResponse(request: EvaluationRequest, responseMap: ResponseMap): Solution {
+        TODO("Not implemented yet")
+    }
 }
