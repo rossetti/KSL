603ff707aab8bb174818a53dff5f5a23daec8eb9
Manuel Rossetti <rossetti@uark.edu>
2025-03-28 08:06:02 -0500

    Update EvaluationRequest.kt

--- KSLCore/src/main/kotlin/ksl/simopt/evaluator/EvaluationRequest.kt
+++ KSLCore/src/main/kotlin/ksl/simopt/evaluator/EvaluationRequest.kt
@@ -1,6 +1,36 @@
 package ksl.simopt.evaluator
 
-class EvaluationRequest(
+import ksl.simopt.problem.InputMap
 
+class EvaluationRequest(
+    numReplications: Int,
+    val inputMap: InputMap
 ) {
+    init {
+        require(numReplications >= 1) {"The number of replications must be >= 1"}
+    }
+
+    var replications: Int = numReplications
+        set(value) {
+            require(value >= 1) {"The number of replications must be >= 1"}
+            field = value
+        }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as EvaluationRequest
+
+        return inputMap == other.inputMap
+    }
+
+    override fun hashCode(): Int {
+        return inputMap.hashCode()
+    }
+
+    val inputValues: DoubleArray
+        get() = inputMap.inputValues
+
+
 }
