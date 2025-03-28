a8a0a634b2370f72fc4a1eb5a3583c37fa65c02e
Manuel Rossetti <rossetti@uark.edu>
2025-03-28 08:42:45 -0500

    Update Solution.kt

--- KSLCore/src/main/kotlin/ksl/simopt/evaluator/Solution.kt
+++ KSLCore/src/main/kotlin/ksl/simopt/evaluator/Solution.kt
@@ -1,4 +1,13 @@
 package ksl.simopt.evaluator
 
-class Solution {
+class Solution(
+    evaluationRequest: EvaluationRequest
+) : Comparable<Solution> {
+
+    //TODO created by Evaluator, can this be a data class
+
+    override fun compareTo(other: Solution): Int {
+        TODO("Not yet implemented")
+    }
+
 }
