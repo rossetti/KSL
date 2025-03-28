221ce57ce7b7b5a994af9c6f9010ce2bc9db3a14
Manuel Rossetti <rossetti@uark.edu>
2025-03-28 14:39:01 -0500

    Update InputMap.kt

--- KSLCore/src/main/kotlin/ksl/simopt/problem/InputMap.kt
+++ KSLCore/src/main/kotlin/ksl/simopt/problem/InputMap.kt
@@ -1,5 +1,13 @@
 package ksl.simopt.problem
 
+/**
+ *  Two InputMaps are considered equal if their (name, value) pairs are the same.
+ *  This class prevents the keys from changing, but allows the changing of
+ *  the data value associated with the keys.
+ *  
+ * @param map the map containing the (name, value) pairs associated with inputs
+ * for the evaluation process.
+ */
 open class InputMap(private val map: MutableMap<String, Double>) : Map<String, Double> by map {
 
     operator fun set(key: String, value: Double) {
