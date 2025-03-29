b19357ffcfc92e34364c7618a1777f20ac66a1f0
Manuel Rossetti <rossetti@uark.edu>
2025-03-29 10:20:08 -0500

    Update Solution.kt

--- KSLCore/src/main/kotlin/ksl/simopt/evaluator/Solution.kt
+++ KSLCore/src/main/kotlin/ksl/simopt/evaluator/Solution.kt
@@ -9,6 +9,11 @@ data class Solution(
     val responseEstimates: List<EstimatedResponse>,
     val responsePenalties: List<Double>
 ) : Comparable<Solution> {
+    
+    init {
+        require(inputMap.isNotEmpty()) { "The input map cannot be empty for a solution" }
+        require(numReplications >= 1) { "The number of replications must be >= 1" }
+    }
 
     //TODO created by Evaluator
 
