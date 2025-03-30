7b9e43467b46067e8fb2ce26ebb8d55b1032c292
Manuel Rossetti <rossetti@uark.edu>
2025-03-30 10:55:47 -0500

    Update ResponseMap.kt

--- KSLCore/src/main/kotlin/ksl/simopt/evaluator/ResponseMap.kt
+++ KSLCore/src/main/kotlin/ksl/simopt/evaluator/ResponseMap.kt
@@ -15,7 +15,7 @@ import ksl.utilities.statistic.Statistic
  *  @param map the map containing the output values for each response
  */
 class ResponseMap(
-    internal val problemDefinition: ProblemDefinition,
+    val problemDefinition: ProblemDefinition,
     private val map: MutableMap<String, MutableList<Double>>
 ) : Map<String, List<Double>> by map {
 
@@ -51,10 +51,17 @@ class ResponseMap(
         val objFnName = problemDefinition.objFnResponseName
         val estimates = estimatedResponses
         val estimatedObjFnc = estimates[objFnName]!!
+        require(request.numReplications == estimatedObjFnc.count.toInt()){
+            "The requested number of replications does not match the number of replications in the response."
+        }
         val responseEstimates = mutableListOf<EstimatedResponse>()
         for ((name, _) in estimates) {
             if (name != objFnName) {
-                responseEstimates.add(estimates[name]!!)
+                val estimate = estimates[name]!!
+                require(request.numReplications == estimate.count.toInt()){
+                    "The requested number of replications does not match the number of replications in the response."
+                }
+                responseEstimates.add(estimate)
             }
         }
         val responsePenalties = responseConstraintPenalties
@@ -77,26 +84,26 @@ class ResponseMap(
         map[key] = list
     }
 
-    /**
-     *  Appends the elements in the supplied list to the list associated
-     *  with the supplied key within the response map. The supplied key
-     *  must already exist in the response map.
-     */
-    fun append(key: String, list: List<Double>){
-        require(map.containsKey(key)) {"The key ($key) is not in the map!"}
-        map[key]!!.addAll(list)
-    }
-
-    /**
-     *  Appends the data associated with the [responses] map into
-     *  the response map. The supplied map of response data must include
-     *  the names associated with this response map. The data from
-     *  the arrays are appended to the data already within the response map.
-     */
-    fun appendAll(responses: Map<String, DoubleArray>){
-        for((name, _) in map){
-            require(responses.containsKey(name)) {"The response $name was not in the supplied map of responses"}
-            append(name, responses[name]!!.asList())
-        }
-    }
+//    /**
+//     *  Appends the elements in the supplied list to the list associated
+//     *  with the supplied key within the response map. The supplied key
+//     *  must already exist in the response map.
+//     */
+//    fun append(key: String, list: List<Double>){
+//        require(map.containsKey(key)) {"The key ($key) is not in the map!"}
+//        map[key]!!.addAll(list)
+//    }
+//
+//    /**
+//     *  Appends the data associated with the [responses] map into
+//     *  the response map. The supplied map of response data must include
+//     *  the names associated with this response map. The data from
+//     *  the arrays are appended to the data already within the response map.
+//     */
+//    fun appendAll(responses: Map<String, DoubleArray>){
+//        for((name, _) in map){
+//            require(responses.containsKey(name)) {"The response $name was not in the supplied map of responses"}
+//            append(name, responses[name]!!.asList())
+//        }
+//    }
 }
