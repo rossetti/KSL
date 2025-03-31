4100ef97a7bf4e3d8454495efc96eb761eb776c6
Manuel Rossetti <rossetti@uark.edu>
2025-03-30 19:00:59 -0500

    Create SimulationProvider.kt

---
+++ KSLCore/src/main/kotlin/ksl/simopt/evaluator/SimulationProvider.kt
@@ -0,0 +1,34 @@
+package ksl.simopt.evaluator
+
+import ksl.controls.experiments.SimulationRunner
+import ksl.simulation.Model
+
+/**
+ *  This simulation provider will execute evaluation requests on the same model
+ *  and collect the desired responses.
+ */
+class SimulationProvider(
+    private val model: Model
+) : SimulationProviderIfc {
+
+    private val mySimulationRunner = SimulationRunner(model)
+
+    override fun runSimulations(cases: Map<EvaluationRequest, ResponseMap>) {
+
+        for ((request, responseMap) in cases){
+            //TODO validate the inputs from the request as valid for the model
+
+            //TODO assign the inputs to the model's inputs
+
+            //TODO setup the experiment to run
+
+            //TODO run the simulation and capture the simulation run
+
+            //TODO extract the desired responses from the simulation responses
+
+            //TODO fill the response map
+        }
+        TODO("Not yet implemented")
+    }
+
+}
