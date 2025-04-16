7b470c4c7192938479166f5615dd7c1070117e8f
Manuel Rossetti <rossetti@uark.edu>
2025-04-16 16:29:01 -0500

    Update StochasticHillClimber.kt

--- KSLCore/src/main/kotlin/ksl/simopt/solvers/algorithms/StochasticHillClimber.kt
+++ KSLCore/src/main/kotlin/ksl/simopt/solvers/algorithms/StochasticHillClimber.kt
@@ -1,8 +1,6 @@
 package ksl.simopt.solvers.algorithms
 
-import ksl.simopt.evaluator.EvaluationRequest
 import ksl.simopt.evaluator.EvaluatorIfc
-import ksl.simopt.evaluator.Solution
 import ksl.simopt.solvers.ReplicationPerEvaluationIfc
 import ksl.simopt.solvers.Solver
 import ksl.utilities.random.rng.RNStreamControlIfc
@@ -10,27 +8,13 @@ import ksl.utilities.random.rng.RNStreamIfc
 import ksl.utilities.random.rvariable.KSLRandom
 
 
-class RestartingStochasticHillClimber(
+class StochasticHillClimber(
     evaluator: EvaluatorIfc,
-    maxEvaluations: Int,
-    maxIterations: Int = 1,
+    maxIterations: Int,
     replicationsPerEvaluation: ReplicationPerEvaluationIfc,
     val rnStream: RNStreamIfc = KSLRandom.defaultRNStream(),
     name: String? = null
 ) : Solver(evaluator, maxIterations, replicationsPerEvaluation, name), RNStreamControlIfc by rnStream {
-    init {
-        require(maxEvaluations > 0) { "maximum number of evaluations must be > 0" }
-    }
-
-    /**
-     *  The maximum number of iterations permitted for the main loop. This must be
-     *  greater than 0.
-     */
-    var maxEvaluations = maxEvaluations
-        set(value) {
-            require(value > 0) { "maximum number of evaluations must be > 0" }
-            field = value
-        }
 
     override fun initializeIterations() {
         val initialPoint = problemDefinition.startingPoint(rnStream)
@@ -39,13 +23,13 @@ class RestartingStochasticHillClimber(
     }
 
     override fun mainIteration() {
-        for (i in 1..maxEvaluations) {
-            // pick point in neighborhood
 
-            // evaluate the solution
+        // generate a random neighbor of the current solution
 
-            // if new solution is better (smaller) update the current solution
-        }
+        // evaluate the solution
+
+        // if new solution is better (smaller) update the current solution
+
     }
 
     override fun isStoppingCriteriaSatisfied(): Boolean {
