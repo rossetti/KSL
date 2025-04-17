a0ea234d6183a2452406771ec0ea8d082e2d581d
Manuel Rossetti <rossetti@uark.edu>
2025-04-17 11:16:06 -0500

    update interfaces

--- KSLCore/src/main/kotlin/ksl/simopt/solvers/GenerateNeighborIfc.kt
+++ KSLCore/src/main/kotlin/ksl/simopt/solvers/GenerateNeighborIfc.kt
@@ -5,6 +5,8 @@ import ksl.simopt.problem.InputMap
 /**
  *   Given input values for a problem, this functional interface should
  *   generate a neighbor relative to the supplied input.
+ *   The solver is supplied to allow potential access to its memory
+ *   within the generation process.
  */
 fun interface GenerateNeighborIfc {
 
@@ -13,8 +15,9 @@ fun interface GenerateNeighborIfc {
      *   generate a neighbor relative to the supplied input.
      *
      *   @param inputMap the input to serve as the basis for determining a neighbor
+     *   @param solver the solver requiring the generated neighbor
      *   @return a neighbor to the supplied input
      */
-    fun generateNeighbor(inputMap: InputMap) : InputMap
+    fun generateNeighbor(inputMap: InputMap, solver: Solver) : InputMap
 
 }
--- KSLCore/src/main/kotlin/ksl/simopt/solvers/NeighborSelectorIfc.kt
+++ KSLCore/src/main/kotlin/ksl/simopt/solvers/NeighborSelectorIfc.kt
@@ -5,6 +5,8 @@ import ksl.simopt.problem.InputMap
 /**
  *  Defines a general functional interface for selecting
  *  a neighbor (point) from a defined neighborhood.
+ *  The solver is supplied to allow potential access to its state/memory
+ *  within the process to determine the neighbor.
  */
 fun interface NeighborSelectorIfc {
 
@@ -12,8 +14,9 @@ fun interface NeighborSelectorIfc {
      *  Defines a general functional interface for selecting
      *  a neighbor (point) from a defined neighborhood.
      *  @param neighborhood the neighborhood from which to select
+     *  @param solver the solver requiring the selected neighbor
      *  @return the selected point from the neighborhood
      */
-    fun select(neighborhood: Set<InputMap>) : InputMap
+    fun select(neighborhood: Set<InputMap>, solver: Solver) : InputMap
 
 }
