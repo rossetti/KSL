c30230d5976ebecb3db50f49107936056bc01d6d
Manuel Rossetti <rossetti@uark.edu>
2025-04-21 12:51:45 -0500

    Update InputDefinition.kt

--- KSLCore/src/main/kotlin/ksl/simopt/problem/InputDefinition.kt
+++ KSLCore/src/main/kotlin/ksl/simopt/problem/InputDefinition.kt
@@ -38,6 +38,25 @@ class InputDefinition(
         require(granularity >= 0.0) { "granularity must be >=  0.0" }
     }
 
+    /**
+     *  Represents the definition of an input variable for a ProblemDefinition.
+     *  Input variables are the variables used in the problem to model the decision parameter
+     *  of the simulation model. The input variable name should correspond to some named parameter
+     *  (e.g. control) in the model.
+     *
+     *  The specified granularity indicates the acceptable precision for the variable's value
+     *  with respect to decision-making. If the granularity is 0 then no rounding will be applied
+     *  when evaluating the variable. Granularity defines the level of precision for an input variable
+     *  to which the problem will be solved. Setting granularity to 0, the default, means that the solver
+     *  will attempt to find a solution to the level of machine precision. For any positive granularity value,
+     *  the solution will be found to some multiple of that granularity. As a special case, setting granularity to 1
+     *  implies an integer-ordered input variable. The specification of granularity reflects a reality for the
+     *  decision maker that there is a level of precision beyond which it is not practical to implement a solution.
+     *
+     *  @param name the name of the input variable
+     *  @param interval the feasible range of the variable as an interval
+     *  @param granularity the acceptable precision for decision-making
+     */
     constructor(
         name: String,
         interval: Interval,
