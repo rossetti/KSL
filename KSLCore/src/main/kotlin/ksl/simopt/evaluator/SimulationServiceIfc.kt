364ae8bb972e6b19fa1c456e3fcd27ad39ea8d92
Manuel Rossetti <rossetti@uark.edu>
2025-04-29 17:06:00 -0500

    Create SimulationServiceIfc.kt

---
+++ KSLCore/src/main/kotlin/ksl/simopt/evaluator/SimulationServiceIfc.kt
@@ -0,0 +1,16 @@
+package ksl.simopt.evaluator
+
+
+/**
+ *  A functional interface that promises to run simulations on
+ *  instances of input/output pairs. The keys of the map are
+ *  evaluation requests for a specific number of replications for
+ *  specific input variable values. The associated ResponseData
+ *  represents the desired responses from the simulation. It should
+ *  contain the replication averages for each desired response.
+ */
+fun interface SimulationServiceIfc {
+
+    fun runSimulations(evaluationRequests: List<InputData>): Map<EvaluationRequest, ResponseData>
+
+}
