04d0f2aea62500427b68970cb0b5c3ac22e78c43
Manuel Rossetti <rossetti@uark.edu>
2025-04-30 16:25:58 -0500

    Update SimulationServiceIfc.kt

--- KSLCore/src/main/kotlin/ksl/simopt/evaluator/SimulationServiceIfc.kt
+++ KSLCore/src/main/kotlin/ksl/simopt/evaluator/SimulationServiceIfc.kt
@@ -4,17 +4,26 @@ import ksl.controls.experiments.SimulationRun
 
 
 /**
- *  A functional interface that promises to run simulations on
+ *  An interface that promises to run simulations on
  *  instances of input/output pairs. The keys of the map are
  *  evaluation requests for a specific number of replications for
  *  specific input variable values. The associated ResponseData
  *  represents the desired responses from the simulation. It should
  *  contain the replication averages for each desired response.
  */
-interface SimulationServiceIfc {
+interface SimulationServiceIfc : SimulationProviderIfc {
 
-    fun runSimulations(requests: List<RequestData>): List<SimulationRun>
+    fun runSimulationRequests(requests: List<RequestData>): List<SimulationRun>
 
+    fun runSimulationRequest(request: RequestData): SimulationRun {
+        return runSimulationRequests(listOf(request)).first()
+    }
 
-
+    //TODO what else can the service do
+    // - indicate if model is registered
+    // - return the response names for a model
+    // - return the experiment run parameters for a model
+    // - return the names of models that are registered
+    // - return the input variable names for a model (variables and rv parameters)
+    //
 }
