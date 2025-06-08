2080e0719fcecf570c61d296c5ebb1fa9770d5f0
Manuel Rossetti <rossetti@uark.edu>
2025-06-08 10:13:47 -0500

    model providers

--- KSLCore/src/main/kotlin/ksl/simulation/ModelProvider.kt
+++ KSLCore/src/main/kotlin/ksl/simulation/ModelProvider.kt
@@ -3,4 +3,16 @@ package ksl.simulation
 /**
  *  A function that promises to provide a model instance
  */
-typealias ModelProvider = () -> Model
+//typealias ModelProvider = () -> Model
+
+/**
+ *  An interface that promises to provide a model instance
+ *  based on some identifier for the model.
+ */
+interface ModelProvider {
+
+    fun isModelProvided(modelIdentifier: String) : Boolean
+
+    fun provideModel(modelIdentifier: String) : Model
+
+}
