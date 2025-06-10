ab6eba77ba72184d8c44a68df05ac67d5edac369
Manuel Rossetti <rossetti@uark.edu>
2025-06-10 14:50:42 -0500

    Update ModelProviderIfc.kt

--- KSLCore/src/main/kotlin/ksl/simulation/ModelProviderIfc.kt
+++ KSLCore/src/main/kotlin/ksl/simulation/ModelProviderIfc.kt
@@ -1,5 +1,7 @@
 package ksl.simulation
 
+import ksl.controls.experiments.ExperimentRunParameters
+
 /**
  * A type alias for a function that creates and returns an instance of the `Model` class.
  */
@@ -18,7 +20,7 @@ interface ModelProviderIfc {
      * @param modelIdentifier the identifier of the model to check availability for
      * @return true if the model corresponding to the given identifier is available, false otherwise
      */
-    fun isModelProvided(modelIdentifier: String) : Boolean
+    fun isModelProvided(modelIdentifier: String): Boolean
 
     /**
      * Provides a model instance based on the given identifier.
@@ -28,7 +30,7 @@ interface ModelProviderIfc {
      * @param modelIdentifier the identifier of the model to retrieve
      * @return the model instance corresponding to the provided identifier
      */
-    fun provideModel(modelIdentifier: String) : Model
+    fun provideModel(modelIdentifier: String): Model
 
     /**
      * Retrieves a list of model identifiers that are available.
@@ -36,7 +38,7 @@ interface ModelProviderIfc {
      * @return a list of strings, each representing an identifier for a model.
      */
     @Suppress("unused")
-    fun modelIdentifiers() : List<String>
+    fun modelIdentifiers(): List<String>
 
     /**
      * Retrieves a list of response names associated with the specified model.
@@ -45,9 +47,30 @@ interface ModelProviderIfc {
      * @return a list of response names corresponding to the specified model
      */
     @Suppress("unused")
-    fun responseNames(modelIdentifier: String) : List<String> {
+    fun responseNames(modelIdentifier: String): List<String> {
         return provideModel(modelIdentifier).responseNames
     }
 
-    fun inputNames(modelIdentifier: String) : List<String>
+    /**
+     * Retrieves the list of input names associated with the specified model.
+     *
+     * @param modelIdentifier the identifier of the model whose input names are to be retrieved
+     * @return a list of strings representing the input names corresponding to the specified model
+     */
+    @Suppress("unused")
+    fun inputNames(modelIdentifier: String): List<String> {
+        return provideModel(modelIdentifier).inputKeys()
+    }
+
+    /**
+     * Retrieves the experimental run parameters for the model identified by the given identifier.
+     * This method extracts detailed configurations and settings required to execute the experiment.
+     *
+     * @param modelIdentifier the identifier of the model whose experimental parameters are to be retrieved
+     * @return an instance of [ExperimentRunParameters] containing the run parameters for the specified model
+     */
+    @Suppress("unused")
+    fun experimentalParameters(modelIdentifier: String) : ExperimentRunParameters{
+        return provideModel(modelIdentifier).extractRunParameters()
+    }
 }
