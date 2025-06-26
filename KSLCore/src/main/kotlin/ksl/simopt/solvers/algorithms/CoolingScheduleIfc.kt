e11401f23c9137f543cc25db783b0a34cfd4f78c
Manuel Rossetti <rossetti@uark.edu>
2025-06-26 12:52:00 -0500

    Update CoolingScheduleIfc.kt

--- KSLCore/src/main/kotlin/ksl/simopt/solvers/algorithms/CoolingScheduleIfc.kt
+++ KSLCore/src/main/kotlin/ksl/simopt/solvers/algorithms/CoolingScheduleIfc.kt
@@ -1,36 +1,59 @@
 package ksl.simopt.solvers.algorithms
 
+import kotlin.math.ln
 import kotlin.math.pow
 
 interface CoolingScheduleIfc {
 
-    fun nextTemperature(currentTemperature: Double): Double
+    fun nextTemperature(iteration: Int): Double
 
 }
 
 class LinearCoolingSchedule(
-    val coolingStep: Double,
+    val initialTemperature: Double,
+    val finalTemperature: Double,
+    val maxIterations: Int
 ) : CoolingScheduleIfc {
     init {
-        require(coolingStep > 0.0) { "Cooling rate must be positive" }
+        require(maxIterations > 0) { "The maximum number of iterations must be positive" }
+        require(initialTemperature > 0.0) { "The initial temperature must be positive" }
+        require(finalTemperature > 0.0) { "The initial temperature must be positive" }
+        require(finalTemperature < initialTemperature) { "The final temperature must be less than the initial temperature" }
     }
 
-    override fun nextTemperature(currentTemperature: Double): Double {
-        require(currentTemperature > 0.0) { "The current temperature must be positive" }
-        return currentTemperature - coolingStep
+    val temperatureDecreasePerIteration = (initialTemperature - finalTemperature) / maxIterations
+
+    override fun nextTemperature(iteration: Int): Double {
+        require(iteration > 0) { "The iteration must be positive" }
+        if (iteration >= maxIterations) return finalTemperature
+        return initialTemperature - (temperatureDecreasePerIteration * iteration)
     }
 }
 
 class ExponentialCoolingSchedule(
+    val initialTemperature: Double,
     val coolingRate: Double = 0.95
 ) : CoolingScheduleIfc {
     init {
+        require(initialTemperature > 0.0) { "The initial temperature must be positive" }
         require(coolingRate > 0.0) { "Cooling rate must be positive" }
         require(coolingRate < 1.0) { "Cooling rate must be less than 1.0" }
     }
 
-    override fun nextTemperature(currentTemperature: Double): Double {
-        require(currentTemperature > 0.0) { "The current temperature must be positive" }
-        return currentTemperature * coolingRate
+    override fun nextTemperature(iteration: Int): Double {
+        require(iteration > 0) { "The iteration must be positive" }
+        return initialTemperature * coolingRate.pow(iteration)
+    }
+}
+
+class LogarithmicCoolingSchedule(
+    val initialTemperature: Double,
+) : CoolingScheduleIfc {
+    init {
+        require(initialTemperature > 0.0) { "The initial temperature must be positive" }
+    }
+
+    override fun nextTemperature(iteration: Int): Double {
+        return initialTemperature / ln(iteration.toDouble() + 1.0)
     }
 }
