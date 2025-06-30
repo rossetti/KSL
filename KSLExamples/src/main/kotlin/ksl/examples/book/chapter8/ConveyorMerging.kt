ee469083522f5368c7eb492b264ea33406eb35b5
Manuel Rossetti <rossetti@uark.edu>
2025-06-30 10:29:37 -0500

    Update ConveyorMerging.kt

--- KSLExamples/src/main/kotlin/ksl/examples/book/chapter8/ConveyorMerging.kt
+++ KSLExamples/src/main/kotlin/ksl/examples/book/chapter8/ConveyorMerging.kt
@@ -11,17 +11,17 @@ class ConveyorMerging(
     name: String? = null
 ) : ProcessModel(parent, name) {
 
-    private val area1 = "Area1"
-    private val area2 = "Area2"
+    private val areaA = "AreaA"
+    private val areaB = "AreaB"
     private val sorting = "Sorting"
-    private val processing = "Processing"
+    private val areaC = "AreaC"
 
     private val conveyor1 = Conveyor.builder(this, "Conveyor1")
         .conveyorType(Conveyor.Type.ACCUMULATING)
         .velocity(1.0)
         .cellSize(1)
         .maxCellsAllowed(1)
-        .firstSegment(area1, sorting, 20)
+        .firstSegment(areaA, sorting, 20)
         .build()
 
     private val conveyor2 = Conveyor.builder(this, "Conveyor2")
@@ -29,7 +29,7 @@ class ConveyorMerging(
         .velocity(1.0)
         .cellSize(1)
         .maxCellsAllowed(1)
-        .firstSegment(area2, sorting, 20)
+        .firstSegment(areaB, sorting, 20)
         .build()
 
     private val conveyor3 = Conveyor.builder(this, "Conveyor3")
@@ -37,37 +37,37 @@ class ConveyorMerging(
         .velocity(1.0)
         .cellSize(1)
         .maxCellsAllowed(1)
-        .firstSegment(sorting, processing, 20)
+        .firstSegment(sorting, areaC, 20)
         .build()
 
     private val tba1 = ExponentialRV(10.0, 1)
-    private val generateArea1Parts = EntityGenerator(this::Area1Part, tba1, tba1)
+    private val generateAreaAParts = EntityGenerator(this::AreaAPart, tba1, tba1)
 
     private val tba2 = TriangularRV(10.0, 16.0, 18.0, 2)
-    private val generateArea2Parts = EntityGenerator(this::Area2Part, tba2, tba2)
+    private val generateAreaBParts = EntityGenerator(this::AreaBPart, tba2, tba2)
 
-    private val myArea1SystemTime = Response(this, "Area1SystemTime")
-    private val myArea2SystemTime = Response(this, "Area2SystemTime")
+    private val myAreaASystemTime = Response(this, "AreaASystemTime")
+    private val myAreaBSystemTime = Response(this, "AreaBSystemTime")
 
-    private inner class Area1Part : Entity() {
+    private inner class AreaAPart : Entity() {
         val mergeProcess: KSLProcess = process(isDefaultProcess = true) {
-            val cr = requestConveyor(conveyor = conveyor1, entryLocation = area1, numCellsNeeded = 1)
+            val cr = requestConveyor(conveyor = conveyor1, entryLocation = areaA, numCellsNeeded = 1)
             rideConveyor(conveyorRequest = cr, destination = sorting)
             val tr = transferTo(conveyorRequest = cr, nextConveyor = conveyor3, entryLocation = sorting)
-            rideConveyor(conveyorRequest = tr, destination = processing)
+            rideConveyor(conveyorRequest = tr, destination = areaC)
             exitConveyor(conveyorRequest = tr)
-            myArea1SystemTime.value = time - entity.createTime
+            myAreaASystemTime.value = time - entity.createTime
         }
     }
 
-    private inner class Area2Part : Entity() {
+    private inner class AreaBPart : Entity() {
         val mergeProcess: KSLProcess = process(isDefaultProcess = true) {
-            val cr = requestConveyor(conveyor = conveyor2, entryLocation = area2, numCellsNeeded = 1)
+            val cr = requestConveyor(conveyor = conveyor2, entryLocation = areaB, numCellsNeeded = 1)
             rideConveyor(conveyorRequest = cr, destination = sorting)
             val tr = transferTo(conveyorRequest = cr, nextConveyor = conveyor3, entryLocation = sorting)
-            rideConveyor(conveyorRequest = tr, destination = processing)
+            rideConveyor(conveyorRequest = tr, destination = areaC)
             exitConveyor(conveyorRequest = tr)
-            myArea2SystemTime.value = time - entity.createTime
+            myAreaBSystemTime.value = time - entity.createTime
         }
     }
 }
