2a6f5f799cb737a363b5f329b7f26cb6ca1edcce
Manuel Rossetti <rossetti@uark.edu>
2025-07-02 11:36:19 -0500

    Update build.gradle.kts

--- KSLProjectTemplate/build.gradle.kts
+++ KSLProjectTemplate/build.gradle.kts
@@ -1,11 +1,9 @@
 
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 // An example gradle build file for a project that depends on the KSL
 
 plugins {
     `java-library`
-    kotlin("jvm") version "1.9.20"
+    kotlin("jvm") version "2.2.0"
 }
 
 repositories {
@@ -17,31 +15,12 @@ dependencies {
 
     // next line allows use of KSL libraries within the project
     // update the release number when new releases become available
-    api(group = "io.github.rossetti", name = "KSLCore", version = "R1.2.0")
-
-
+    api(group = "io.github.rossetti", name = "KSLCore", version = "R1.2.1")
+  //  api(group = "io.github.rossetti", name = "KSLCore", version = "R1.2.0")
     testImplementation(kotlin("test"))
     implementation(kotlin("stdlib-jdk8"))
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "17"
-}
-
-tasks.jar {
-    exclude("logback.xml")
-}
-
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "17"
-}
-
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "17"
+kotlin {
+    jvmToolchain(21)
 }
