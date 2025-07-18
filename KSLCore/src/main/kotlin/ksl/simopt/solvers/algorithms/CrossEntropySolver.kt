0e79a03d731cf9a4161dfbb558ea35d88322ddbb
Manuel Rossetti <rossetti@uark.edu>
2025-07-18 16:34:48 -0500

    CE initial testing not good
    printing not working, CE stopping rules not working

--- KSLCore/src/main/kotlin/ksl/simopt/solvers/algorithms/CrossEntropySolver.kt
+++ KSLCore/src/main/kotlin/ksl/simopt/solvers/algorithms/CrossEntropySolver.kt
@@ -184,6 +184,7 @@ class CrossEntropySolver @JvmOverloads constructor(
         currentSolution = myEliteSolutions.first()
         // capture the last solution
         myLastSolutions.addLast(currentSolution)
+        println("Iteration: $iterationCounter  CE: $currentSolution")
     }
 
     override fun isStoppingCriteriaSatisfied(): Boolean {
