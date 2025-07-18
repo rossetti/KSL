10e8b6cd3535b3377632864ac7dde5e895bff39c
Manuel Rossetti <rossetti@uark.edu>
2025-07-18 13:51:42 -0500

    Create CircularQueue.kt

---
+++ KSLCore/src/main/kotlin/ksl/utilities/collections/CircularQueue.kt
@@ -0,0 +1,46 @@
+package ksl.utilities.collections
+
+/**
+ * A circular queue is a linear data structure in which the operations are performed based on FIFO
+ * (First In First Out) principle, and the last position is connected back to the first position
+ * to make a circle. It is also called “Ring Buffer.”
+ */
+class CircularQueue<T>(private val maxSize: Int) {
+    private val elements: Array<T?> = arrayOfNulls(maxSize)
+    private var front = 0
+    private var rear = -1
+    private var currentSize = 0
+
+    fun enqueue(item: T): Boolean {
+        if (isFull()) {
+            return false // Queue is full
+        }
+        rear = (rear + 1) % maxSize
+        elements[rear] = item
+        currentSize++
+        return true
+    }
+
+    fun dequeue(): T? {
+        if (isEmpty()) {
+            return null // Queue is empty
+        }
+        val item = elements[front]
+        elements[front] = null // Optional: clear the dequeued element
+        front = (front + 1) % maxSize
+        currentSize--
+        return item
+    }
+
+    fun isFull(): Boolean {
+        return currentSize == maxSize
+    }
+
+    fun isEmpty(): Boolean {
+        return currentSize == 0
+    }
+
+    fun size(): Int {
+        return currentSize
+    }
+}
