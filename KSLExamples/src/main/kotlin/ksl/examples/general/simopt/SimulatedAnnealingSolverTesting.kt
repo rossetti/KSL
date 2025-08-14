2087cf74c8deb2c04dcad522e6799a7e58f1ae50
Manuel Rossetti <rossetti@uark.edu>
2025-08-14 14:40:35 -0500

    revised tests

--- KSLExamples/src/main/kotlin/ksl/examples/general/simopt/SimulatedAnnealingSolverTesting.kt
+++ KSLExamples/src/main/kotlin/ksl/examples/general/simopt/SimulatedAnnealingSolverTesting.kt
@@ -1,10 +1,6 @@
 package ksl.examples.general.simopt
 
-
-import ksl.simopt.evaluator.Evaluator
-import ksl.simopt.evaluator.Solution
 import ksl.simopt.solvers.Solver
-import ksl.simopt.solvers.algorithms.SimulatedAnnealing
 
 
 fun main() {
@@ -12,54 +8,24 @@ fun main() {
   //  val modelIdentifier = "RQInventoryModel"
     val modelIdentifier = "LKInventoryModel"
     val initialTemperature = 1000.0
-    runSASolverTest(modelIdentifier, initialTemperature, maxIterations = 10)
-}
-
-fun configureSimulatedAnnealingSolver(
-    evaluator: Evaluator,
-    initialTemperature: Double,
-    maxIterations: Int = 100,
-    replicationsPerEvaluation: Int = 50,
-    printer: ((Solver) -> Unit)? = null
-): SimulatedAnnealing {
-    val shc = SimulatedAnnealing(
-        evaluator,
-        initialTemperature,
-        maxIterations = maxIterations, replicationsPerEvaluation = replicationsPerEvaluation
+    val problemDefinition = makeProblemDefinition(modelIdentifier)
+    val modelBuilder = selectBuilder(modelIdentifier)
+    val printer = selectPrinter(modelIdentifier)
+    val solver = Solver.simulatedAnnealingSolver(
+        problemDefinition = problemDefinition,
+        modelBuilder = modelBuilder,
+        startingPoint = null,
+        initialTemperature = initialTemperature,
+        maxIterations = 10,
+        replicationsPerEvaluation = 50,
+        printer = printer,
     )
-    printer?.let { shc.emitter.attach(it) }
-    return shc
-}
-
-fun runSimulatedAnnealingSolver(
-    evaluator: Evaluator,
-    initialTemperature: Double,
-    inputs: MutableMap<String, Double>,
-    maxIterations: Int = 100,
-    replicationsPerEvaluation: Int = 50,
-    printer: ((Solver) -> Unit)? = null
-) {
-    val shc = configureSimulatedAnnealingSolver(evaluator, initialTemperature,
-        maxIterations, replicationsPerEvaluation, printer)
-    shc.startingPoint = evaluator.problemDefinition.toInputMap(inputs)
-    shc.runAllIterations()
+    solver.runAllIterations()
     println()
     println("Solver Results:")
-    println(shc)
+    println(solver)
     println()
     println("Final Solution:")
-    println(shc.bestSolution.asString())
-}
+    println(solver.bestSolution.asString())
 
-fun runSASolverTest(
-    modelIdentifier: String,
-    initialTemperature: Double,
-    maxIterations: Int = 100
-) {
-    val inputs = createInputs(modelIdentifier)
-    val evaluator = makeEvaluator(modelIdentifier)
-    val printer = if (modelIdentifier == "RQInventoryModel") ::printRQInventoryModel else ::printLKInventoryModel
-    runSimulatedAnnealingSolver(evaluator, initialTemperature,
-        inputs, maxIterations, printer = printer)
 }
-
