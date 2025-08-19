ba64631e0f76b8edd18ab398e6bb9c0687be4525
Manuel Rossetti <rossetti@uark.edu>
2025-08-19 11:37:58 -0500

    Create EvaluationRequest.kt

---
+++ KSLCore/src/main/kotlin/ksl/simopt/evaluator/EvaluationRequest.kt
@@ -0,0 +1,38 @@
+package ksl.simopt.evaluator
+
+import kotlinx.serialization.Serializable
+
+/**
+ *  An evaluation request is used to request simulation oracle execution on a model.
+ *
+ *  @param modelIdentifier the identifier for the model that will execute the requests
+ *  @param requests the request (input/output) for the model to use
+ *  @param crnOption indicates if the requests should be executed by the model using
+ *  common random numbers. The default is false.
+ */
+@Serializable
+data class EvaluationRequest(
+    val modelIdentifier: String,
+    val requests: List<RequestData>,
+    val crnOption: Boolean = false
+) {
+    init {
+        require(requests.isNotEmpty()) { "At least one request must be defined" }
+        if (requests.size == 1) {
+            require(crnOption == false) {"The crnOption must be false when there is only one request" }
+        }
+        for (request in requests) {
+            require(request.modelIdentifier == modelIdentifier) { "Model identifier of requests must match the model identifier of the request" }
+        }
+    }
+
+    /**
+     *  A convenience constructor for making an evaluation request that has a single request.
+     *  The CRN option is false by default because it is a single request. CRN is applied
+     *  across multiple evaluations.
+     */
+    constructor(modelIdentifier: String, request: RequestData,) : this(
+        modelIdentifier = modelIdentifier,
+        requests = listOf(request),
+    )
+}
