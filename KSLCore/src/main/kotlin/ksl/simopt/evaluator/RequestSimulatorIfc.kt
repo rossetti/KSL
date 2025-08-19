3fd4d1a7be8195429c9dea8c05bc6d1926e30656
Manuel Rossetti <rossetti@uark.edu>
2025-08-19 12:22:03 -0500

    remove crn interface

--- KSLCore/src/main/kotlin/ksl/simopt/evaluator/RequestSimulatorIfc.kt
+++ KSLCore/src/main/kotlin/ksl/simopt/evaluator/RequestSimulatorIfc.kt
@@ -3,23 +3,6 @@ package ksl.simopt.evaluator
 interface RequestSimulatorIfc {
 
     /**
-     *  Control whether the evaluator switches to the use of common random numbers (CRN).
-     *  When the evaluator starts using CRN, it will continue to use CRN until told
-     *  not to use CRN.
-     *
-     * @param modelIdentifier the model identifier of the model for application of CRN
-     *  @param crnOption if true, the evaluator should start using common random numbers
-     *  when evaluating requests.
-     */
-    fun useCommonRandomNumbers(modelIdentifier: String, crnOption: Boolean)
-
-    /**
-     * Returns true if the CRN option is on for the identified model.
-     * @param modelIdentifier the model identifier of the model for application of CRN
-     */
-    fun crnOption(modelIdentifier: String) : Boolean
-
-    /**
      * Executes multiple simulations based on the provided list of request data and maps each request
      * to a corresponding ResponseMap. Each request is processed individually, and the results of the
      * simulations are stored as a key-value pair in the returned map. If the input list is empty, an
