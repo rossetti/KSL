f47a0cd05453ab9c6cfc02e1c7fa0d96b0c7acdd
Manuel Rossetti <rossetti@uark.edu>
2025-08-23 16:58:50 -0500

    need to test SimulationProvider and SimulationService

--- KSLCore/src/main/kotlin/ksl/simopt/evaluator/SimulationServiceV2.kt
+++ KSLCore/src/main/kotlin/ksl/simopt/evaluator/SimulationServiceV2.kt
@@ -2,25 +2,16 @@ package ksl.simopt.evaluator
 
 import ksl.controls.experiments.SimulationRun
 import ksl.simopt.cache.SimulationRunCacheIfc
+import ksl.simopt.evaluator.SimulationProvider.Companion.captureResultFromSimulationRun
 import ksl.simopt.evaluator.SimulationProvider.Companion.executeSimulation
 import ksl.simulation.Model
 import ksl.simulation.ModelDescriptor
 
 abstract class SimulationServiceV2(
-    simulationRunCache: SimulationRunCacheIfc? = null
+    val simulationRunCache: SimulationRunCacheIfc? = null
 ) : SimulationServiceIfcV2 {
 
     protected abstract fun provideModel(modelIdentifier: String): Model?
-
-    /**
-     *  The simulation provider for this service. This is used to execute simulations.
-     *  The simulation provider is created by the service when it is instantiated.
-     *  The provider is configured with the simulation run cache, if it exists.
-     */
-    protected val mySimulationProvider: SimulationProvider = SimulationProvider(Model(), simulationRunCache)
-
-    val simulationRunCache: SimulationRunCacheIfc?
-        get() = mySimulationProvider.simulationRunCache
 
     abstract fun modelIdentifiers(): Set<String>
 
@@ -67,13 +58,80 @@ abstract class SimulationServiceV2(
             val failure = Result.failure<SimulationRun>(ModelNotProvidedException(msg))
             return mapOf(modelInputs to failure)
         }
-        //TODO need to handle caching and CRN because this could be multiple runs on the same model
-        TODO("Not yet implemented")
+        val originalExpRunParams = model.extractRunParameters()
+        // The evaluation request has options for caching and CRN that need to be handled
+        if (evaluationRequest.crnOption || !evaluationRequest.cachingAllowed || (simulationRunCache == null)) {
+            // CRN should not permit cache retrieval. There is no way to ensure that the simulation runs
+            // in the cache used CRN and saving dependent samples in the cache is problematic.
+            // If there is no caching allowed or there isn't a cache, we just do the simulations.
+            if (evaluationRequest.crnOption) {
+                model.resetStartStreamOption = true
+            }
+            val results = simulateWithoutCache(evaluationRequest.modelInputs, model)
+            model.changeRunParameters(originalExpRunParams)
+            return results
+        }
+        // There is a cache and we are permitted to use it.
+        val results = mutableMapOf<ModelInputs, Result<SimulationRun>>()
+        for (modelInputs in evaluationRequest.modelInputs) {
+            var simulationRun = useCachedSimulationRun(modelInputs)
+            if (simulationRun == null) {
+                simulationRun = executeSimulation(modelInputs, model)
+            }
+             results[modelInputs] = if (simulationRun.runErrorMsg.isEmpty()) {
+                 Result.success(simulationRun)
+             } else {
+                 SimulationServiceIfc.logger.info { "SimulationService: Simulation for model: ${model.name} experiment: ${model.experimentName} had an error. " }
+                 SimulationServiceIfc.logger.info { "Error message: ${simulationRun.runErrorMsg} " }
+                 Result.failure(SimulationRunException(simulationRun))
+             }
+        }
+        model.changeRunParameters(originalExpRunParams)
+        return results
+    }
+
+    private fun useCachedSimulationRun(modelInputs: ModelInputs): SimulationRun? {
+        if (simulationRunCache == null) return null
+        if (simulationRunCache.containsKey(modelInputs)) {
+            // check if it has the appropriate number of replications
+            val requestedReplications = modelInputs.numReplications
+            val simulationRun = simulationRunCache[modelInputs]!!
+            if (requestedReplications <= simulationRun.numberOfReplications) {
+                return simulationRun
+            }
+        }
+        return null
+    }
+
+    private fun simulateWithoutCache(
+        modelInputs: List<ModelInputs>,
+        model: Model
+    ) : Map<ModelInputs, Result<SimulationRun>> {
+        val results = mutableMapOf<ModelInputs, Result<SimulationRun>>()
+        for (modelInputs in modelInputs) {
+            val simulationRun = executeSimulation(modelInputs, model)
+            results[modelInputs] = if (simulationRun.runErrorMsg.isEmpty()) {
+                Result.success(simulationRun)
+            } else {
+                SimulationServiceIfc.logger.info { "SimulationService: Simulation for model: ${model.name} experiment: ${model.experimentName} had an error. " }
+                SimulationServiceIfc.logger.info { "Error message: ${simulationRun.runErrorMsg} " }
+                Result.failure(SimulationRunException(simulationRun))
+            }
+        }
+        return results
     }
 
     override fun simulate(evaluationRequest: EvaluationRequest): Map<ModelInputs, Result<ResponseMap>> {
-        //This should call runSimulations(evaluationRequest: EvaluationRequest): Map<ModelInputs, Result<SimulationRun>>
-        // and then translate the simulation runs to response maps.
-        TODO("Not yet implemented")
+         // translate the simulation runs to response maps.
+        val simulations = runSimulations(evaluationRequest)
+        val allResults = mutableMapOf<ModelInputs, Result<ResponseMap>>()
+        for ((modelInputs, simulationRunResult) in simulations) {
+            simulationRunResult.onFailure {
+                allResults[modelInputs] = Result.failure(it)
+            } .onSuccess {
+                allResults[modelInputs] = captureResultFromSimulationRun(modelInputs, it)
+            }
+         }
+        return allResults
     }
 }
