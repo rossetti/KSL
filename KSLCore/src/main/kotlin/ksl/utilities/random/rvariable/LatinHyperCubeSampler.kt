ba170d704d899410b8196fed754dd3648910185e
Manuel Rossetti <rossetti@uark.edu>
2025-08-29 15:18:04 -0500

    latin hyper cube testing

--- KSLCore/src/main/kotlin/ksl/utilities/random/rvariable/LatinHyperCubeSampler.kt
+++ KSLCore/src/main/kotlin/ksl/utilities/random/rvariable/LatinHyperCubeSampler.kt
@@ -4,6 +4,16 @@ import ksl.utilities.Interval
 import ksl.utilities.random.permute
 import ksl.utilities.random.rng.RNStreamProviderIfc
 
+/**
+ *  Creates a Latin hyper-cube sampler with each dimension divided into the specified number of points.
+ *  The hyper-cube is formed from the specified intervals.
+ *
+ *  @param pointsPerDimension the number of divisions of the dimensions
+ *  @param intervals the intervals forming the "cube"
+ * @param streamNum the random number stream number, defaults to 0, which means the next stream
+ * @param streamProvider the provider of random number streams, defaults to [KSLRandom.DefaultRNStreamProvider]
+ * @param name an optional name
+ */
 class LatinHyperCubeSampler(
     val pointsPerDimension: Int,
     val intervals: List<Interval>,
@@ -12,25 +22,43 @@ class LatinHyperCubeSampler(
     name: String? = null
 ) : MVRVariable(streamNum, streamProvider, name) {
 
+    val width: Double
+
+    private val myCachedPoints = Array(pointsPerDimension) { DoubleArray(dimension) { 0.0 } }
+
+    private var myCurrentPoint = 0
+
     init {
         require(pointsPerDimension > 0) { "The number of points per dimension must be greater than zero!" }
         require(intervals.isNotEmpty()) { "The intervals must not be empty!" }
         for (interval in intervals) {
             require(interval.lowerLimit.isFinite()) { "The interval must be finite. The lower limit was -Infinity." }
             require(interval.upperLimit.isFinite()) { "The interval must be finite. The upper limit was +Infinity." }
-            require(interval.lowerLimit != interval.upperLimit) { "The interval's width must be greater than zero!" }
+            require(interval.lowerLimit < interval.upperLimit) { "The interval's width must be greater than zero!" }
         }
+        width = 1.0/pointsPerDimension
         generateCachedPoints()
     }
 
+    /**
+     *  Creates a Latin hyper-cube sampler over the unit hyper-cube of the specified dimension.
+     *
+     *  @param pointsPerDimension the number of divisions of the dimensions
+     *  @param dimension number of dimensions for the unit hyper-cube.
+     * @param streamNum the random number stream number, defaults to 0, which means the next stream
+     * @param streamProvider the provider of random number streams, defaults to [KSLRandom.DefaultRNStreamProvider]
+     * @param name an optional name
+     */
+    constructor(
+        pointsPerDimension: Int,
+        dimension: Int,
+        streamNum: Int = 0,
+        streamProvider: RNStreamProviderIfc = KSLRandom.DefaultRNStreamProvider,
+        name: String? = null
+    ) : this(pointsPerDimension, createZeroOneIntervals(dimension), streamNum, streamProvider, name)
+
     override val dimension: Int
         get() = intervals.size
-
-    val width = 1.0 / pointsPerDimension
-
-    private val myCachedPoints = Array(pointsPerDimension) { DoubleArray(dimension) { 0.0 } }
-
-    private var myCurrentPoint = 0
 
     private fun generateCachedPoints() {
         val myTmp = DoubleArray(pointsPerDimension)
@@ -40,7 +68,7 @@ class LatinHyperCubeSampler(
             }
             myTmp.permute(rnStream)
             for (j in 0 until pointsPerDimension) {
-                myCachedPoints[i][j] = myTmp[j]
+                myCachedPoints[j][i] = myTmp[j]
             }
         }
     }
@@ -65,6 +93,25 @@ class LatinHyperCubeSampler(
         rnStreamProvider: RNStreamProviderIfc
     ): LatinHyperCubeSampler {
         return LatinHyperCubeSampler(this.pointsPerDimension, this.intervals, streamNumber, streamProvider, name)
+    }
+
+    companion object {
+
+        /**
+         *  Creates [numIntervals] intervals over the range from 0 to 1.
+         *  @param numIntervals the number of intervals. Must be at least 1.
+         *  @return the list of created intervals
+         */
+        fun createZeroOneIntervals(numIntervals: Int): List<Interval> {
+            require(numIntervals > 0) { "The number of intervals must be positive." }
+            val list = mutableListOf<Interval>()
+            val interval = Interval(0.0, 1.0)
+            for(i in 0 until numIntervals) {
+                list.add(interval)
+                println(interval)
+            }
+            return list
+        }
     }
 
 }
