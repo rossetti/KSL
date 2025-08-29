a4982156413ee4d8cf09fdb596b2725627211604
Manuel Rossetti <rossetti@uark.edu>
2025-08-29 17:02:49 -0500

    added to StartingPointIfc
    can generate a set of feasible points

--- KSLCore/src/main/kotlin/ksl/simopt/problem/StartingPointIfc.kt
+++ KSLCore/src/main/kotlin/ksl/simopt/problem/StartingPointIfc.kt
@@ -6,5 +6,20 @@ fun interface StartingPointIfc {
         problemDefinition: ProblemDefinition,
     ): InputMap
 
+    /**
+     *  Generates a set of input feasible points by repeatedly calling startingPoint()
+     *  and checking if the point is feasible.
+     */
+    fun generateInputFeasiblePoints(numPoints: Int, problemDefinition: ProblemDefinition): Set<InputMap> {
+        require(numPoints > 0) { "The number of points must be > 0" }
+        val set = mutableSetOf<InputMap>()
+        while(set.size <= numPoints) {
+            val point = startingPoint(problemDefinition)
+            if (problemDefinition.isInputFeasible(point)) {
+                set.add(point)
+            }
+        }
+        return set
+    }
 }
 
