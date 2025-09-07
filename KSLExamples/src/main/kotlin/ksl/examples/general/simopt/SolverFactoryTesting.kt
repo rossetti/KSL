a69ce64d4909216c73a09d7a1ba75e2f28713a41
Manuel Rossetti <rossetti@uark.edu>
2025-09-07 17:00:03 -0500

    Update SolverFactoryTesting.kt
    RSPLINE issue in piecewiseLinearInterpolation, cannot run CRN when there is only 1 feasible input

--- KSLExamples/src/main/kotlin/ksl/examples/general/simopt/SolverFactoryTesting.kt
+++ KSLExamples/src/main/kotlin/ksl/examples/general/simopt/SolverFactoryTesting.kt
@@ -24,12 +24,12 @@ enum class SolverType {
 
 fun main() {
 //    var simulationRunCache = MemorySimulationRunCache()
-    //  val modelIdentifier = "RQInventoryModel"
-    val modelIdentifier = "LKInventoryModel"
-//    val solverType = SolverType.R_SPLINE
+      val modelIdentifier = "RQInventoryModel"
+//    val modelIdentifier = "LKInventoryModel"
+    val solverType = SolverType.R_SPLINE
 //    val solverType = SolverType.CE
 //    val solverType = SolverType.SHC
-    val solverType = SolverType.R_SPLINE_RS
+//    val solverType = SolverType.R_SPLINE_RS
 //    val solverType = SolverType.SA_RS
 //        val solverType = SolverType.SHC_RS
     runSolver(modelIdentifier, solverType, defaultKSLDatabaseObserverOption = false)
