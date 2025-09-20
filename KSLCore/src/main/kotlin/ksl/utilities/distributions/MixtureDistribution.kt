8525b6274cbf6d59f9912a1961d53490fd6ffe5e
Manuel Rossetti <rossetti@uark.edu>
2025-09-20 08:56:09 -0500

    Update MixtureDistribution.kt

--- KSLCore/src/main/kotlin/ksl/utilities/distributions/MixtureDistribution.kt
+++ KSLCore/src/main/kotlin/ksl/utilities/distributions/MixtureDistribution.kt
@@ -205,20 +205,3 @@ class MixtureDistribution(
     }
 
 }
-
-fun main() {
-    // test mixture
-    val list = listOf<ContinuousDistributionIfc>(Normal(), Exponential())
-    val cdf = doubleArrayOf(0.5, 1.0)
-    val mixtureDistribution = MixtureDistribution(list, cdf)
-    // test p = 0.75
-    val p = 0.75
-    val quantile = mixtureDistribution.invCDF(p)
-    println("quantile = $quantile")
-    val mean = mixtureDistribution.mean()
-    val variance = mixtureDistribution.variance()
-    println("mean = $mean")
-    println("variance = $variance")
-    val parameters = mixtureDistribution.parameters()
-    println("parameters = ${parameters.joinToString(",")}")
-}
