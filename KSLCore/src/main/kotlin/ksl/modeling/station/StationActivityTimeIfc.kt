d367953a2cc3ba05ea2abb82b1b4c9b57d79890d
Manuel Rossetti <rossetti@uark.edu>
2025-10-12 11:31:39 -0500

    Update StationActivityTimeIfc.kt

--- KSLCore/src/main/kotlin/ksl/modeling/station/StationActivityTimeIfc.kt
+++ KSLCore/src/main/kotlin/ksl/modeling/station/StationActivityTimeIfc.kt
@@ -2,6 +2,9 @@ package ksl.modeling.station
 
 import ksl.simulation.ModelElement.QObjectIfc
 
+/**
+ *  A functional interface for determining the activity time at a station
+ */
 fun interface StationActivityTimeIfc {
 
     fun activityTime(qObjectIfc: QObjectIfc, station: Station) : Double
