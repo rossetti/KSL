19c8f5d828e2499f754d9a5f5b7ea80514252ad3
Manuel Rossetti <rossetti@uark.edu>
2025-12-26 13:28:17 -0500

    jar work

--- KSLCore/src/main/kotlin/ksl/utilities/io/JARModelBuilder.kt
+++ KSLCore/src/main/kotlin/ksl/utilities/io/JARModelBuilder.kt
@@ -3,17 +3,25 @@ package ksl.utilities.io
 import ksl.simulation.ExperimentRunParametersIfc
 import ksl.simulation.Model
 import ksl.simulation.ModelBuilderIfc
+import ksl.utilities.io.DynamicJarClassLoader
 import java.nio.file.Path
 import java.nio.file.Paths
 
+/**
+ *
+ */
 class JARModelBuilder(
     jarPath: Path,
     modelBuilderClassName: String? = null
-) : ModelBuilderIfc {
+) : ModelBuilderIfc , AutoCloseable {
 
-    private val myLoader = DynamicJarClassLoader(jarPath)
+    private var myBuilder: ModelBuilderIfc? = null
+
+    val isBuildable: Boolean
+        get() = myBuilder != null
 
     init {
+        val myLoader = DynamicJarClassLoader(jarPath)
         val modelBuilderClass : Class<*> = if (modelBuilderClassName != null) {
             require(modelBuilderClassName.isNotBlank()) { "ModelBuilder class name cannot be blank" }
             require(myLoader.classNames.contains(modelBuilderClassName)) { "ModelBuilder class name: $modelBuilderClassName is not in the JAR file. " }
@@ -32,18 +40,20 @@ class JARModelBuilder(
             val c : Class<*>  = modelBuilders.first()
             c
         }
-
         // first try to load it as a singleton that implements the interface
         var builder = myLoader.singletonObjectReference(modelBuilderClass.name)
         if (builder == null) {
             // if not a singleton then try to create it from a no argument constructor
+            builder = myLoader.noArgumentInstance(modelBuilderClass)
         }
+        require(builder != null) { "Unable to instantiate $modelBuilderClassName builder instance" }
         // if not a singleton then try to create it from a no argument constructor
-
+        myBuilder = builder as ModelBuilderIfc
+        myLoader.close()
     }
 
     /**
-     * Constructor for a single JAR file
+     * Constructor for a JAR file
      */
     constructor(jarPath: String) : this(Paths.get(jarPath))
 
@@ -52,7 +62,12 @@ class JARModelBuilder(
         experimentRunParameters: ExperimentRunParametersIfc?,
         defaultKSLDatabaseObserverOption: Boolean
     ): Model {
-        TODO("Not yet implemented")
+        require(myBuilder != null) {"The model builder is not available for building" }
+        return myBuilder!!.build(modelConfiguration, experimentRunParameters, defaultKSLDatabaseObserverOption)
+    }
+
+    override fun close() {
+        myBuilder = null
     }
 
 }
