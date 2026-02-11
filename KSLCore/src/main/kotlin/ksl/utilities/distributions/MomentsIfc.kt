7fbe34a2fa41e235863706d4d55086d9a5b89b87
Manuel Rossetti <rossetti@uark.edu>
2026-02-11 16:00:43 -0600

    Update MomentsIfc.kt

--- KSLCore/src/main/kotlin/ksl/utilities/distributions/MomentsIfc.kt
+++ KSLCore/src/main/kotlin/ksl/utilities/distributions/MomentsIfc.kt
@@ -1,10 +1,30 @@
 package ksl.utilities.distributions
 
+import kotlin.math.sqrt
+
 interface MomentsIfc {
 
     val mean: Double
     val variance: Double
     val skewness: Double
     val kurtosis: Double
+
+    val secondMoment: Double
+        get() {
+            val m = mean
+            return variance + m * m
+        }
+
+    val thirdMoment: Double
+        get() {
+            val v = variance
+            return skewness * v * sqrt(v)
+        }
+
+    val fourthMoment: Double
+        get() {
+            val v = variance
+            return kurtosis * v * v
+        }
 
 }
