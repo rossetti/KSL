7dbf043556dad4fe809ce616c6ce4d0847baf2c1
Manuel Rossetti <rossetti@uark.edu>
2026-05-10 07:32:07 -0500

    Update CoolingScheduleIfc.kt

--- KSLCore/src/main/kotlin/ksl/simopt/solvers/algorithms/CoolingScheduleIfc.kt
+++ KSLCore/src/main/kotlin/ksl/simopt/solvers/algorithms/CoolingScheduleIfc.kt
@@ -40,6 +40,7 @@ interface CoolingScheduleIfc {
  */
 abstract class CoolingSchedule(initialTemperature: Double) : CoolingScheduleIfc {
     init {
+        require(initialTemperature.isFinite()) {"The initial temperature must be finite."}
         require(initialTemperature > 0.0) { "The initial temperature must be positive" }
     }
 
@@ -71,13 +72,17 @@ class LinearCoolingSchedule(
 ) : CoolingSchedule (initialTemperature) {
     init {
         require(maxIterations > 0) { "The maximum number of iterations must be positive" }
+        require(initialTemperature.isFinite()) {"The initial temperature must be finite."}
+        require(initialTemperature > 0.0) { "The initial temperature must be positive" }
+        require(stoppingTemperature.isFinite()) {"The stopping temperature must be finite."}
         require(stoppingTemperature > 0.0) { "The initial temperature must be positive" }
-        require(stoppingTemperature < initialTemperature) { "The final temperature must be less than the initial temperature" }
+        require(stoppingTemperature < initialTemperature) { "The stopping temperature must be less than the initial temperature" }
     }
 
     override var initialTemperature: Double = initialTemperature
         set(value) {
             require(value > 0.0) { "The initial temperature must be positive" }
+            require(value.isFinite()) {"The initial temperature must be finite."}
             require(value > stoppingTemperature) { "Initial temperature must be greater than stopping temperature" }
             field = value
         }
