32b66a24cb2ed18ecddae3f0c5a873f69669ab32
Manuel Rossetti <rossetti@uark.edu>
2026-07-18 11:09:57 -0500

    Update build.gradle.kts

--- KSLProjectTemplate/build.gradle.kts
+++ KSLProjectTemplate/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
 
     // next line allows use of KSL libraries within the project
     // update the release number when new releases become available
-    api("io.github.rossetti:KSLCore:R1.3")
+    api("io.github.rossetti:KSLCore:R1.4")
     testImplementation(kotlin("test"))
     implementation(kotlin("stdlib-jdk8"))
 }
